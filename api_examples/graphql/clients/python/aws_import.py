9bfd59bb56548fbeb9b517aa5e8dc493932eef49
Omero Saienni <omero@turbot.com>
2020-04-02 18:55:45 +0100

    Added Python AWS Account import example

---
+++ api_examples/graphql/clients/python/aws_import.py
@@ -0,0 +1,102 @@
+import click
+import os
+import turbot
+from sgqlc.endpoint.http import HTTPEndpoint
+
+# -c .config/turbot/credentials.yml --profile env --parent 100000000000000 --account 900000000000 --role_arn arn:aws:iam::900000000000:role/turbot_service_role --external_id 50000000
+@click.command()
+@click.option('-c', '--config-file', type=click.Path(dir_okay=False), help="Pass an optional yaml config file.")
+@click.option('-p', '--profile', default="default", help="Profile to be used from config file.")
+@click.option('--parent', required=True, help="The resource id for the parent folder of this subscription")
+@click.option('--account', required=True, help="The AWS account ID")
+@click.option('--role_arn', required=True, help="The role ARN to be assumed into the account")
+@click.option('--external_id', required=True, help="The external ID used to validate the assume role")
+def run_controls(config_file, profile, parent, account, role_arn, external_id):
+    """Import an AWS Account"""
+
+    config = turbot.Config(config_file, profile)
+    headers = {'Authorization': 'Basic {}'.format(config.auth_token)}
+    endpoint = HTTPEndpoint(config.graphql_endpoint, headers)
+
+    aws_mutation = '''
+      mutation CreateAwsAccount($input: CreateResourceInput!) {
+          createResource(input: $input) {
+            turbot {
+              id
+          }
+        }
+      }
+    '''
+
+    credentials_mutation = '''
+      mutation SetAwsAccountPolicies($setRoleArn: CreatePolicySettingInput!, $setExternalId: CreatePolicySettingInput!) {
+        roleArn: createPolicySetting(input: $setRoleArn) {
+          turbot {
+            id
+          }
+        }
+        externalId: createPolicySetting(input: $setExternalId) {
+          turbot {
+            id
+          }
+        }
+      }
+    '''
+
+    """
+    The parent variable holds the resource ID of the folder where this subscription will be imported.
+    """
+    aws_variables = {
+        "input": {
+            "parent": parent,
+            "type": "tmod:@turbot/aws#/resource/types/account",
+            "data": {
+                "Id": account
+            },
+            "metadata": {
+                "aws": {
+                    "accountId": account,
+                    # NOTE: gov-cloud not yet supported #
+                    "partition": "aws"
+                }
+            }
+        }
+    }
+
+    print("Importing account: {}".format(account))
+    account_response = endpoint(aws_mutation, aws_variables)
+    if "errors" in account_response:
+        return report_graphql_error("aws_mutation", account_response["errors"])
+
+    account_rid = account_response['data']['createResource']['turbot']['id']
+    print("Created Account Resource ID: {}".format(account_rid))
+
+    credentials_variables = {
+        "setRoleArn": {
+            "type": "tmod:@turbot/aws#/policy/types/turbotIamRole",
+            "resource": account_rid,
+            "value": role_arn,
+            "precedence": "REQUIRED"
+        },
+        "setExternalId": {
+            "type": "tmod:@turbot/aws#/policy/types/turbotIamRoleExternalId",
+            "resource": account_rid,
+            "value": external_id,
+            "precedence": "REQUIRED"
+        }
+    }
+    creds_response = endpoint(credentials_mutation, credentials_variables)
+    if "errors" in creds_response:
+        return report_graphql_error("credentials_mutation", creds_response["errors"])
+
+    print("Import complete")
+
+
+def report_graphql_error(section, errors):
+    print("Error(s) detected when running mutation: {}".format(section))
+    for error in errors:
+        print(error["message"])
+
+
+if __name__ == "__main__":
+    run_controls()
--- api_examples/graphql/clients/python/turbot.py
+++ api_examples/graphql/clients/python/turbot.py
@@ -4,6 +4,7 @@ from xdg import XDG_CONFIG_HOME
 import yaml
 import requests
 
+
 class Config:
     """ Locates the users Turbot credentials, verifies connectivity to
         the specified Turbot workspace and instantiates a config object. """
@@ -15,7 +16,7 @@ class Config:
         turbot_config = "{}/turbot/credentials.yml".format(XDG_CONFIG_HOME)
         graphql_path = 'api/latest/graphql'
         health_path = 'api/latest/turbot/health'
-        
+
         # Option 1: Use custom yaml configuration file.
         if custom_config_file:
             with open(custom_config_file, 'r') as stream:
@@ -27,17 +28,21 @@ class Config:
         # Option 2: Use environment variables
         elif (os.getenv("TURBOT_ACCESS_KEY_ID") and os.getenv("TURBOT_SECRET_ACCESS_KEY")):
             config_dict[config_profile] = {}
-            config_dict[config_profile]['accessKey'] = os.getenv("TURBOT_ACCESS_KEY_ID")
-            config_dict[config_profile]['secretKey'] = os.getenv("TURBOT_SECRET_ACCESS_KEY")
+            config_dict[config_profile]['accessKey'] = os.getenv(
+                "TURBOT_ACCESS_KEY_ID")
+            config_dict[config_profile]['secretKey'] = os.getenv(
+                "TURBOT_SECRET_ACCESS_KEY")
 
             # Option 2a: Use TURBOT_WORKSPACE environment variable
             if os.getenv("TURBOT_WORKSPACE"):
-                config_dict[config_profile]['workspace'] = os.getenv("TURBOT_WORKSPACE")
+                config_dict[config_profile]['workspace'] = os.getenv(
+                    "TURBOT_WORKSPACE")
 
             # Option 2b: Use TURBOT_GRAPHQL_ENDPOINT environment variable
             elif os.getenv("TURBOT_GRAPHQL_ENDPOINT"):
                 if os.getenv("TURBOT_GRAPHQL_ENDPOINT").endswith(graphql_path):
-                    config_dict[config_profile]['workspace'] = os.getenv("TURBOT_GRAPHQL_ENDPOINT")[:-len(graphql_path)]
+                    config_dict[config_profile]['workspace'] = os.getenv(
+                        "TURBOT_GRAPHQL_ENDPOINT")[:-len(graphql_path)]
                 else:
                     config_fail = "Incorrect TURBOT_GRAPHQL_ENDPOINT format, exiting..."
             else:
@@ -54,33 +59,38 @@ class Config:
         if len(config_fail):
             print(config_fail)
             exit()
-        
+
         if config_dict:
 
             if not config_profile in config_dict:
                 config_fail = "No matching profile, exiting..."
-            
-            if not all (k in config_dict[config_profile] for k in ("workspace","accessKey","secretKey")):
+
+            if not all(k in config_dict[config_profile] for k in ("workspace", "accessKey", "secretKey")):
                 config_fail = "Incorrect config file format, exiting..."
 
             if len(config_fail):
                 print(config_fail)
                 exit()
-            
+
             self.workspace = config_dict[config_profile]['workspace']
             self.accessKey = config_dict[config_profile]['accessKey']
             self.secretKey = config_dict[config_profile]['secretKey']
             # Set and Test Endpoints
             if self.workspace.endswith("/"):
-                self.graphql_endpoint = "{}{}".format(self.workspace, graphql_path)
-                self.health_endpoint = "{}{}".format(self.workspace, health_path)
+                self.graphql_endpoint = "{}{}".format(
+                    self.workspace, graphql_path)
+                self.health_endpoint = "{}{}".format(
+                    self.workspace, health_path)
             else:
-                self.graphql_endpoint = "{}/{}".format(self.workspace, graphql_path)
-                self.health_endpoint = "{}/{}".format(self.workspace, health_path)
-            
+                self.graphql_endpoint = "{}/{}".format(
+                    self.workspace, graphql_path)
+                self.health_endpoint = "{}/{}".format(
+                    self.workspace, health_path)
+
             print("Testing connection to {} ...".format(self.workspace))
             self.test_health()
-            auth_bytes = '{}:{}'.format(self.accessKey, self.secretKey).encode("utf-8")
+            auth_bytes = '{}:{}'.format(
+                self.accessKey, self.secretKey).encode("utf-8")
             self.auth_token = b64encode(auth_bytes).decode()
 
         else:
