2d1d198f91e6e5092c2924d002a22ff63a4ea480
Colson Hoxie <colson@turbot.com>
2020-04-03 14:12:35 -0700

    removed VA mention, adjusted readme text at bottom

--- control_objectives/aws_rds_db_approved/README.md
+++ control_objectives/aws_rds_db_approved/README.md
@@ -17,4 +17,4 @@ To run the S3 Example:
 - Run `terraform plan -var-file="default.tfvars"` and review the changes to be applied
 - Run `terraform apply -var-file="default.tfvars"` to execute and apply the policy settings
 
-> The baseline runs with the default values. However, you could create and set your own defaults using `.tfvars` file that will override the existing files.
+> The terraform plan runs with default values. Ensure the policy values are correct in main.tf before applying the plan.
--- control_objectives/aws_rds_db_approved/main.tf
+++ control_objectives/aws_rds_db_approved/main.tf
@@ -2,7 +2,7 @@
 
 # AWS > RDS > DB Instance > Approved 
  resource "turbot_policy_setting" "rds_db_approved" {
-  resource = turbot_smart_folder.va_rds.id
+  resource = turbot_smart_folder.rds_instance_approved.id
   type = "tmod:@turbot/aws-rds#/policy/types/dbInstanceApproved"
   value = "Check: Approved"
 
@@ -11,7 +11,7 @@
 #RDS Engines approved (Define engines, aurora, mariaDB, postgreSQL here as examples)
 # AWS > RDS > DB Instance > Approved > Database Engines
  resource "turbot_policy_setting" "rds_db_approved_engine" {
-  resource = turbot_smart_folder.va_rds.id
+  resource = turbot_smart_folder.rds_instance_approved.id
   type = "tmod:@turbot/aws-rds#/policy/types/dbInstanceApprovedDatabaseEngines"
   value = yamlencode(["aurora","mariaDB","postgreSQL"])
 }
@@ -19,7 +19,7 @@
 #RDS Instance Classes (Define instances, db.t3.* and db.m5.2xlarge as examples)
 # AWS > RDS > DB Instance > Approved > Instance Classes
  resource "turbot_policy_setting" "rds_db_approved_instancetypes" {
-  resource = turbot_smart_folder.va_rds.id
+  resource = turbot_smart_folder.rds_instance_approved.id
   type = "tmod:@turbot/aws-rds#/policy/types/dbInstanceApprovedInstanceClasses"
   value = yamlencode(["db.t3.*","db.m5.2xlarge"])
 }
