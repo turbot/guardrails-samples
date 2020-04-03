3b5e6b3c3aa9fbddf30302baf67671c517a5b064
Colson Hoxie <colson@turbot.com>
2020-04-03 16:08:44 -0700

    final edit

--- control_objectives/aws_vpc_flow_logs/main.tf
+++ control_objectives/aws_vpc_flow_logs/main.tf
@@ -1,14 +1,13 @@
-resource "turbot_smart_folder" "baseline" {
+resource "turbot_smart_folder" "vpc_flow_logging" {
   title         = var.smart_folder_title
-  description   = "Create a smart folder to apply the s3 policy settings"
+  description   = "Create a smart folder to apply the Flow Log policy settings"
   parent        = "tmod:@turbot/turbot#/"
 }
 
-### Decision on CW log group or S3 and Traffic(Accept&Rejects or just one?)
-
-### Setting VPC Flow logging for the VPC.
+# Setting VPC Flow logging for the VPC.
 # AWS > VPC > VPC > Flow Logging
 # Alternate values include:
+
 /*
 - Skip
 - Check: Configured per `Flow Logging > *`
@@ -18,7 +17,7 @@ resource "turbot_smart_folder" "baseline" {
 */
 
 resource "turbot_policy_setting" "vpc_flow_logs" {
-    resource = turbot_smart_folder.va_logging.id
+    resource = turbot_smart_folder.vpc_flow_logging.id
     type = "tmod:@turbot/aws-vpc-core#/policy/types/vpcFlowLogging"
     value = "Check: Not configured"
 }
