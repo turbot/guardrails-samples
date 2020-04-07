ea816e07d641ad3c2938a1d5d1913dd597b98386
Colson Hoxie <colson@turbot.com>
2020-04-07 10:15:56 -0700

    removed quotes, edited policy values to validate correctly

--- control_objectives/aws_rds_db_approved/main.tf
+++ control_objectives/aws_rds_db_approved/main.tf
@@ -14,12 +14,13 @@ resource "turbot_smart_folder" "rds_engine_instance_type_approved" {
 
 }
 
-#RDS Engines approved (Define engines, aurora, mariaDB, postgreSQL here as examples)
+#RDS Engines approved (Define engines, aurora, mariaDB, postgreSQL here as examples). ENGINES MUST BE LOWER CASE
 # AWS > RDS > DB Instance > Approved > Database Engines
+
  resource "turbot_policy_setting" "rds_db_approved_engine" {
   resource = turbot_smart_folder.rds_engine_instance_type_approved.id
   type = "tmod:@turbot/aws-rds#/policy/types/dbInstanceApprovedDatabaseEngines"
-  value = yamlencode(["aurora","mariaDB","postgreSQL"])
+  value = yamlencode(["aurora","mariadb","postgresql"])
 }
 
 #RDS Instance Classes (Define instances, db.t3.* and db.m5.2xlarge as examples)
