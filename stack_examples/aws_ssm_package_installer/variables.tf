0b55613b8e462edb19687f97a1f635ae38d1f387
Omero Saienni <39497847+omerosaienni@users.noreply.github.com>
2020-05-20 12:58:33 +0100

    Update variables.tf

--- stack_examples/aws_ssm_package_installer/variables.tf
+++ stack_examples/aws_ssm_package_installer/variables.tf
@@ -1,9 +1,9 @@
 variable "region_resource" {
-  description = "Enter the Turbot Resource ARN for the AWS Region where SSM Documents will be placed. I.e.: 'arn:aws::us-east-1:999999999999'"
+  description = "Enter the Turbot Resource ARN for the AWS Region where SSM Documents will be placed. For example 'arn:aws::us-east-1:999999999999'"
   type    = string
 }
 
 variable "ssm_document_role" {
-  description = "Enter the AWS IAM Role ARN to be used by SSM to run documents. I.e.: 'arn:aws:iam::999999999999:role/AmazonSSMRoleForAutomationAssumeQuickSetup'"
+  description = "Enter the AWS IAM Role ARN to be used by SSM to run documents. For example 'arn:aws:iam::999999999999:role/AmazonSSMRoleForAutomationAssumeQuickSetup'"
   type    = string
 }
