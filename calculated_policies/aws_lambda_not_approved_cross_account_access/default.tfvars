7f01732f7caaeaebe0bde4c62f0413e75ed2da84
luis cavalcante <luis@turbot.com>
2020-05-26 08:11:47 -0300

    Restrict Lambda Cross Account Access #132

---
+++ calculated_policies/aws_lambda_not_approved_cross_account_access/default.tfvars
@@ -0,0 +1,4 @@
+smart_folder_title = "AWS Lambda Cross Account Access"
+
+# Examples for target_resource: "tmod:@turbot/turbot#/" or "187486019045335"
+target_resource = "<resource_id or aka>"
---
+++ calculated_policies/aws_lambda_not_approved_cross_account_access/main.tf
@@ -0,0 +1,54 @@
+resource "turbot_smart_folder" "aws_lambda_not_approved_cross_account_access" {
+  title          = var.smart_folder_title
+  description    = "Restrict AWS Lambda Cross Account Access"
+  parent         = "tmod:@turbot/turbot#/"
+}
+
+resource "turbot_smart_folder_attachment" "aws_lambda_not_approved_cross_account_access" {
+  resource     = var.target_resource
+  smart_folder = turbot_smart_folder.aws_lambda_not_approved_cross_account_access.id
+}
+
+# AWS > Lambda > Function > Approved
+resource "turbot_policy_setting" "aws_lambda_function_approved" {
+  resource = turbot_smart_folder.aws_lambda_not_approved_cross_account_access.id
+  type = "tmod:@turbot/aws-lambda#/policy/types/functionApproved"
+  value = "Check: Approved"
+}
+
+# AWS > Lambda > Function > Approved > Usage
+resource "turbot_policy_setting" "aws_lambda_not_approved_cross_account_access" {
+  resource       = turbot_smart_folder.aws_lambda_not_approved_cross_account_access.id
+  type           = "tmod:@turbot/aws-lambda#/policy/types/functionApprovedUsage"
+  # GraphQL to pull function metadata
+  template_input = <<EOT
+  {
+    resource {
+      iamPolicies: get(path: "IamPolicy.Policy.Statement")
+      turbot {
+        custom
+      }
+    }
+  }
+  EOT
+  # Nunjucks Template Nunjucks Comments are formatted: {# comment #}
+  template       = <<EOT
+  {% set has_cross_account = false -%}
+  {% for iamPolicy in $.resource.iamPolicies -%}
+  {%   if iamPolicy.Principal.AWS and iamPolicy.Principal.AWS.split(':')[4] != $.resource.turbot.custom.aws.accountId -%}
+  {%     set has_cross_account = true -%}
+  {%   endif -%}
+  {%   if iamPolicy.Condition.StringEquals['AWS:SourceAccount'] and iamPolicy.Condition.StringEquals['AWS:SourceAccount'] != $.resource.turbot.custom.aws.accountId -%}
+  {%     set has_cross_account = true -%}
+  {%   endif -%}
+  {%   if iamPolicy.Condition.ArnLike['AWS:SourceArn'] and iamPolicy.Condition.ArnLike['AWS:SourceArn'].split(':')[4] != $.resource.turbot.custom.aws.accountId -%}
+  {%     set has_cross_account = true -%}
+  {%   endif -%}
+  {% endfor -%}
+  {% if has_cross_account -%}
+      "Not approved"
+  {%- else -%}
+      "Approved"
+  {%- endif -%}
+  EOT
+}
