a6e24fa101ad5fb2d5c8df90a1a6b652f5e4a2bc
Luis Cavalcante <luis@turbot.com>
2020-05-29 15:44:06 -0300

    Create a Calculated Policy to set Instance Approved if using Local AMI #153 (#164)

---
+++ calculated_policies/aws_ec2_instance_approved_usage_local_ami/variables.tf
@@ -0,0 +1,9 @@
+variable "smart_folder_title" {
+  type        = string
+  description = "Enter a title for the smart folder"
+}
+
+variable "target_resource" {
+  type        = string
+  description = "Enter a target_resource to attach the smart folder and apply its policies."
+}
