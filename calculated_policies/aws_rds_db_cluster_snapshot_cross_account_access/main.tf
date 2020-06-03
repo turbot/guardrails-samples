8b7a533d911c7d7cab6cf0439d25672378a4a3a6
Omero Saienni <39497847+omerosaienni@users.noreply.github.com>
2020-06-03 15:59:03 +0100

    Calculated Policy - AWS > RDS > DB Cluster Snapshot [Manual] Cross Account Access #181 (#182)

---
+++ calculated_policies/aws_rds_db_cluster_snapshot_cross_account_access/main.tf
@@ -0,0 +1,43 @@
+resource "turbot_smart_folder" "rds_smart_folder" {
+  title       = var.smart_folder_title
+  description = var.smart_folder_description
+  parent      = var.smart_folder_parent_resource
+}
+
+resource "turbot_policy_setting" "rds_approved_usage_policy_setting" {
+  resource       = turbot_smart_folder.rds_smart_folder.id
+  type           = "tmod:@turbot/aws-rds#/policy/types/dbClusterSnapshotManualApprovedUsage"
+  template_input = <<EOF
+  {
+    dbClusterSnapshotManual 
+    {
+      sharedAccounts: get(path:"DBClusterSnapshotAttributes.AttributeValues")
+    }
+  }
+  EOF
+  template       = <<EOF
+  {#- Whitelist of account that are approved for snapshot usage -#}
+  {%- set whitelist = ["${join("\" ,\"", var.approved_accounts)}"] -%}
+  {%- set approvalCount = 0 -%}
+
+  {%- for sharedAccount in $.dbClusterSnapshotManual.sharedAccounts | sort -%}
+    {%- for validAccount in whitelist | sort -%}
+      {%- if validAccount == sharedAccount -%}
+        {%- set approvalCount = approvalCount + 1 -%}
+      {%- endif -%}
+    {%- endfor -%}
+  {%- endfor -%}
+
+  {%- if approvalCount ==  $.dbClusterSnapshotManual.sharedAccounts | length -%}
+    "Approved"
+  {%- else -%}
+    "Not approved"
+  {%- endif -%}
+  EOF
+  precedence     = "REQUIRED"
+}
+
+resource "turbot_smart_folder_attachment" "rds_smart_folder_attachment" {
+  resource     = var.target_resource
+  smart_folder = turbot_smart_folder.rds_smart_folder.id
+}
