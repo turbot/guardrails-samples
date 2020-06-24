6236137d36280f5eabff9937a0d10eb435021010
Ben Cornelius <ben.cornelius@tempus.com>
2020-06-24 17:15:44 -0500

    Update documentation

--- baselines/gcp/gcp_project_import/main.tf
+++ baselines/gcp/gcp_project_import/main.tf
@@ -4,22 +4,22 @@ resource "turbot_resource" "project_resource" {
   type   = "tmod:@turbot/gcp#/resource/types/project"
   metadata = jsonencode({
     "gcp" : {
-      "projectId" : var.gcp_project_id
+      "projectId" : var.gcp_project_id # this is client_id in the pem that GCP gives you in your service account's JSON private key
     }
   })
   data = jsonencode({
-    "projectId" : var.gcp_project_id
+    "projectId" : var.gcp_project_id # this is client_id in the pem that GCP gives you in your service account's JSON private key
   })
 }
 
 resource "turbot_policy_setting" "clientEmail" {
-  resource = var.parent_resource
+  resource = turbot_resource.project_resource.id
   type     = "tmod:@turbot/gcp#/policy/types/clientEmail"
   value    = var.client_email
 }
 
 resource "turbot_policy_setting" "privateKey" {
-  resource = var.parent_resource
+  resource = turbot_resource.project_resource.id
   type     = "tmod:@turbot/gcp#/policy/types/privateKey"
   value    = var.private_key
 }
