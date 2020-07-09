9742aa99accb28b6b2d81d56ead95f3ff4f3c474
Joe Michela <joe@turbot.com>
2020-07-08 22:15:25 -0400

    Baselines to enable smart retention with the default values.

---
+++ baselines/turbot/smart_retention/variables.tf
@@ -0,0 +1,25 @@
+variable "enforce_smart_retention" {
+  description = "Enter the name for the local directory to be created:"
+  type        = bool
+  default =  true
+}
+
+variable "min_retention" {
+  type = number
+  default = 14
+}
+
+variable "max_retention" {
+  type = number
+  default = 365
+}
+
+variable "purge_limit" {
+  type = number
+  default = 30
+}
+
+variable "debug_logs" {
+  type = number
+  default = 14
+}
