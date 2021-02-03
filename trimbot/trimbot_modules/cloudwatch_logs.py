064c3d9bc5ab840e555230343ae04fd482e198b4
omero <omero@turbot.com>
2021-02-03 14:55:58 +0000

    Send back to alex

--- trimbot/trimbot_modules/cloudwatch_logs.py
+++ trimbot/trimbot_modules/cloudwatch_logs.py
@@ -10,28 +10,30 @@ class CloudwatchLogGroup(Resource):
 
         super().__init__(session)
 # what does this do?
+
     def details(self):
         return f'cloudwatch/log {self.logs["Name"]}'
 
     def delete(self, dry_run):
-        logger = logging.getLogger(__name__)
-        log_client = self.session.create_client('logs', self.region)
+        pass
+#         logger = logging.getLogger(__name__)
+#         log_client = self.session.create_client('logs', self.region)
 
-        response = log_client.describe_log_groups()
+#         response = log_client.describe_log_groups()
 
-# adds list of groups from response to array and deletes/prints log groups to window
-        groups = []
-        for group in response['logGroups']:
-            if not dry_run:
-                groups.append(group['logGroupName'])
-            else:
-                logger.info(f"Would remove log group {group['logGroupName']})
+# # adds list of groups from response to array and deletes/prints log groups to window
+#         groups = []
+#         for group in response['logGroups']:
+#             if not dry_run:
+#                 groups.append(group['logGroupName'])
+#             else:
+#                 logger.info(f"Would remove log group {group['logGroupName']})
 
-        if not dry_run:
-            log_client.delete_log_group(logGroupName=self.logs["Name"])
-            logger.info(f"Deleted log group {self.logs['Name']}")
-        else:
-            logger.info(f"Would delete log group {self.logs['Name']}")
+#         if not dry_run:
+#             log_client.delete_log_group(logGroupName=self.logs["Name"])
+#             logger.info(f"Deleted log group {self.logs['Name']}")
+#         else:
+#             logger.info(f"Would delete log group {self.logs['Name']}")
 
 
 class CloudWatchLogsResourceService(ResourceService):
@@ -39,16 +41,18 @@ class CloudWatchLogsResourceService(ResourceService):
         super().__init__(session, recipe, "event", "rule")
 
     def get_all(self, region):
-        name_prefix = []
-        for filter in self.recipe["filters"]:
-            if filter["type"] == "startswith":
-                name_prefix = filter["value"]
 
-        log_client = self.session.create_client('logs', region)
+        return []
+        # name_prefix = []
+        # for filter in self.recipe["filters"]:
+        #     if filter["type"] == "startswith":
+        #         name_prefix = filter["value"]
 
-        response = log_client.list_rules(NamePrefix=name_prefix)
+        # log_client = self.session.create_client('logs', region)
 
-        return [CloudwatchLogGroup(self.session, region, rule) for rule in response["Rules"]]
+        # response = log_client.list_rules(NamePrefix=name_prefix)
+
+        # return [CloudwatchLogGroup(self.session, region, rule) for rule in response["Rules"]]
 
 
 #    - name: "Cloudwatch Log Groups - Delete"
@@ -56,7 +60,7 @@ class CloudWatchLogsResourceService(ResourceService):
 #       resource: LogGroups
 #       filters:
 #         - type: startswith
-#           value: 
+#           value:
 #             - /aws/lambda/turbot_ban
 #             - /turbot/ban
 #       actions:
