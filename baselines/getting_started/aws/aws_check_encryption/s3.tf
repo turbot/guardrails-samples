b029916ba53e25aa8b7171444c1820b4fc0c3597
raj <raj@turbot.com>
2021-02-03 23:27:22 +0530

    Initial commit for AWS check encryption.Closes #367

--- baselines/getting_started/aws/aws_check_encryption/s3.tf
+++ baselines/getting_started/aws/aws_check_encryption/s3.tf
@@ -1,6 +1,10 @@
-# Ensure encryption in transit and at rest on S3 Buckets
-# Note: these are also part of the S3 Baseline as well
+# Encryption at Rest Guardrails - https://turbot.com/v5/docs/concepts/guardrails/encryption-at-rest
+# Encryption in Transit Guardrails - https://turbot.com/v5/docs/concepts/guardrails/encryption-in-transit
 
+# Encryption at Rest and In Transit.  Also in the Encryption Baseline
+
+# AWS > S3 > Bucket > Encryption at Rest
+# https://turbot.com/v5/mods/turbot/aws-s3/inspect#/control/types/bucketEncryptionAtRest
 
 resource "turbot_policy_setting" "s3_encryption_at_rest" {
     resource        = turbot_smart_folder.aws_encryption.id
@@ -8,6 +12,8 @@ resource "turbot_policy_setting" "s3_encryption_at_rest" {
     value           = "Check: AWS SSE or higher"
 }
 
+# AWS > S3 > Bucket > Encryption in Transit
+# https://turbot.com/v5/mods/turbot/aws-s3/inspect#/control/types/encryptionInTransit
 resource "turbot_policy_setting" "s3_encryption_in_transit" {
     resource        = turbot_smart_folder.aws_encryption.id
     type            = "tmod:@turbot/aws-s3#/policy/types/encryptionInTransit"
