88df871cb32687968d463ce50a56e34d128b9c71
raj <raj@turbot.com>
2021-02-04 13:06:22 +0530

    Added refactored files.

---
+++ baselines/getting_started/aws/aws_check_encryption/outputs.tf
@@ -0,0 +1,11 @@
+output "turbot_profile" {
+  value = var.turbot_profile
+}
+
+output "smart_folder_name" {
+  value = var.smart_folder_name
+}
+
+output "smart_folder_parent_resource" {
+  value = var.smart_folder_parent_resource
+}
