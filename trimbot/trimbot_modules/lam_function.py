601d3d3859cb034bcf163c3b10562be6f49fe849
Alex Smith <fatalbert3390@gmail.com>
2021-02-09 10:55:25 -0500

    fixed references and tidied up

--- trimbot/trimbot_modules/lam_function.py
+++ trimbot/trimbot_modules/lam_function.py
@@ -4,8 +4,8 @@ import logging
 
 
 class LambdaFunction(Resource):
-    def __init__(self, session, region, function) -> None:
-        self.function = function
+    def __init__(self, session, region, function_name) -> None:
+        self.function_name = function_name
         self.region = region
         self.lambda_client = session.create_client('lambda', self.region)
         self.logger = logging.getLogger(__name__)
@@ -13,20 +13,21 @@ class LambdaFunction(Resource):
         super().__init__(session)
 
     def details(self):
-        return f'lambda/function {self.function["FunctionName"]}'
+        return f'lambda/function {self.function_name}'
 
-    # def delete(self, dry_run):
-    #     if not dry_run:
-    #         self.lambda_client.delete_function(FunctionName=self.function["FunctionName"])
-    #         self.logger.info(f"Removed function {self.function["FunctionName"]}")
-    #     else:
-    #         self.logger.info(f"Would remove function {self.function["FunctionName"]}")
+    def delete(self, dry_run):
+        if not dry_run:
+            self.lambda_client.delete_function(FunctionName=self.function_name)
+            self.logger.info(f"Removed function {self.function_name}")
+        else:
+            self.logger.info(f"Would remove function {self.function_name}")
 
 
 class LambdaFunctionResourceService(ResourceService):
     def __init__(self, session, recipe) -> None:
+        self.function_names = self.create_function_names(session)
         super().__init__(session, recipe, "lambda", "function")
-
+        
     def create_function_names(self, session):
 
         lambda_client = session.create_client("lambda", session.get_default_region())
@@ -37,16 +38,16 @@ class LambdaFunctionResourceService(ResourceService):
         for response in response_iterator:
             results += response.get('Functions')
 
-        return [Functions['FunctionName'] for function in results]
+        return [function['FunctionName'] for function in results]
 
 
     def get_all(self, region):
         
-        for function_name in results:
+        resources = []
+        for function_name in self.function_names:
             for filter in self.recipe["filters"]:
-                if filter["type"] == "NamePrefix":
+                if filter["type"] == "startswith":
                     for value in filter["value"]:
-                        if function["FunctionName"].startswith(value):
-                            resources.append(FunctionName(self.session, region, function))
-
-        return []
+                        if function_name.startswith(value):
+                            resources.append(LambdaFunction(self.session, region, function_name))
+        return resources
