0ed16c11dfb7644c318e9ab7ab6d8ef085f793eb
omero <omero@turbot.com>
2021-02-10 15:50:30 +0000

    Removed smart folder

--- baselines/notifications/security-hub/smart_folder.tf
+++
@@ -1,5 +0,0 @@
-resource "turbot_smart_folder" "baseline_security_hub" {
-  parent      = var.smart_folder_parent_resource
-  title       = var.smart_folder_name
-  description = var.smart_folder_description
-}
--- baselines/notifications/security-hub/variables.tf
+++ baselines/notifications/security-hub/variables.tf
@@ -24,24 +24,3 @@ variable "aws_profile" {
   type        = string
   default     = "default"
 }
-
-variable "smart_folder_name" {
-  description = "Smart folder name for the baseline"
-  type        = string
-  default     = "SecurityHub Policies"
-}
-
-variable "smart_folder_description" {
-  description = "Enter a description for the smart folder"
-  type        = string
-  default     = <<-DEFAULT
-  Defines the sets of policies require to configure Turbot to push out notifications.
-  These notifications will be transformed and persisted as SecurityHub findings.
-  DEFAULT
-}
-
-variable "smart_folder_parent_resource" {
-  description = "Enter the resource ID or AKA for the parent resource of the smart folder"
-  type        = string
-  default     = "tmod:@turbot/turbot#/"
-}
