b93bb946c69a21c5f0d7e48a3560035e2701aad2
omero <omero@turbot.com>
2021-02-10 10:47:20 +0000

    Variables coming online

--- baselines/notifications/security-hub/providers.tf
+++ baselines/notifications/security-hub/providers.tf
@@ -4,6 +4,10 @@ terraform {
     turbot = {
       source = "turbot/turbot"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
   required_version = ">= 0.13"
 }
@@ -15,25 +19,4 @@ provider "turbot" {
 provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
-}
-
-variable "smart_folder_name" {
-  description = "Smart folder name for the baseline"
-  type        = string
-  default     = "SecurityHub Policies"
-}
-
-variable "smart_folder_description" {
-  description = "Enter a description for the smart folder"
-  type        = string
-  default     = <<-DEFAULT
-  Defines the sets of policies require to configure Turbot to push out notifications.
-  These notifications will be transformed and persisted as SecurityHub findings.
-  DEFAULT
-}
-
-variable "smart_folder_parent_resource" {
-  description = "Enter the resource ID or AKA for the parent resource of the smart folder"
-  type        = string
-  default     = "tmod:@turbot/turbot#/"
 }
--- baselines/notifications/security-hub/variables.tf
+++ baselines/notifications/security-hub/variables.tf
@@ -1,5 +1,12 @@
+variable "aws_region" {
+  description = "Configures which AWS region SecurityHub baseline resources are created"
+  type        = string
+}
+
 variable "turbot_profile" {
   description = "Turbot profile used to install policies for a workspace managed by the profile"
+  type        = string
+  default     = "default"
 }
 
 
@@ -9,7 +16,23 @@ variable "aws_profile" {
   default     = "default"
 }
 
-variable "aws_region" {
-  description = "Configures which AWS region SecurityHub baseline resources are created"
+variable "smart_folder_name" {
+  description = "Smart folder name for the baseline"
   type        = string
+  default     = "SecurityHub Policies"
+}
+
+variable "smart_folder_description" {
+  description = "Enter a description for the smart folder"
+  type        = string
+  default     = <<-DEFAULT
+  Defines the sets of policies require to configure Turbot to push out notifications.
+  These notifications will be transformed and persisted as SecurityHub findings.
+  DEFAULT
+}
+
+variable "smart_folder_parent_resource" {
+  description = "Enter the resource ID or AKA for the parent resource of the smart folder"
+  type        = string
+  default     = "tmod:@turbot/turbot#/"
 }
