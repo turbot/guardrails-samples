bf41f8ce098b993169011817a172772be79d0fbb
krishna5891 <krishna@turbot.com>
2021-02-15 23:38:42 +0530

    Baseline Refactor Azure_Check_Stack

--- baselines/getting_started/azure/azure_check_stack/sub_monitor_stack_policies.tf
+++ baselines/getting_started/azure/azure_check_stack/sub_monitor_stack_policies.tf
@@ -1,4 +1,7 @@
 ## Set policy to deploy example Azure Subscription Stack
+
+# Azure > Subscription > Stack
+# https://turbot.com/v5/mods/turbot/azure/inspect#/policy/types/subscriptionStack
 resource "turbot_policy_setting" "azure_subscription_monitor_stack" {
   resource = turbot_smart_folder.azure_stack.id
   type     = "tmod:@turbot/azure#/policy/types/subscriptionStack"
@@ -6,18 +9,22 @@ resource "turbot_policy_setting" "azure_subscription_monitor_stack" {
   #value    = "Enforce: Configured"
 }
 
+# Azure > Subscription > Stack > Terraform Version
+# https://turbot.com/v5/mods/turbot/azure/inspect#/policy/types/subscriptionStackTerraformVersion
 # Sets the Terraform version for your Source
 resource "turbot_policy_setting" "azure_subscription_monitor_stack_tfversion" {
   resource = turbot_smart_folder.azure_stack.id
   type     = "tmod:@turbot/azure#/policy/types/subscriptionStackTerraformVersion"
-  value    = "0.12.*"
+  value    = "0.13.*"
 }
 
+# Azure > Subscription > Stack > Source
+# https://turbot.com/v5/mods/turbot/azure/inspect#/policy/types/subscriptionStackSource
 ## Set policy to apply the Stack Source policy, the TF file source
 resource "turbot_policy_setting" "azure_subscription_monitor_stack_source" {
   resource       = turbot_smart_folder.azure_stack.id
   type           = "tmod:@turbot/azure#/policy/types/subscriptionStackSource"
   value           = <<-SOURCE
-    ${file("./tf_includes/sourcestack.tf")}
+    ${file("./tf_includes/sourcestack_policies.tf")}
     SOURCE
 }
---
+++ baselines/getting_started/azure/azure_check_stack/variables.tf
@@ -0,0 +1,21 @@
+variable "turbot_profile" {
+  description = "Enter profile matching your turbot cli credentials."
+}
+
+variable "smart_folder_name" {
+  description = "Smart folder name for the baseline"
+  type        = string
+  default     = "Azure Stack Example Policies"
+}
+
+variable "smart_folder_description" {
+  description = "Enter a description for the smart folder"
+  type        = string
+  default     = "Defines sets of policies for the Azure Stack baseline"
+}
+
+variable "smart_folder_parent_resource" {
+  description = "Enter the resource ID or AKA for the parent of the smart folder"
+  type        = string
+  default     = "tmod:@turbot/turbot#/"
+} 
