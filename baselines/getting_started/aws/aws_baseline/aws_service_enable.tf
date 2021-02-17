bf5979055ea95fed9960300b99e46fea032d9ebd
Rajesh Mohanty <raj@turbot.com>
2021-02-17 13:41:45 +0530

    Baseline - Refactor AWS baseline. Closes #365 (#408)

--- baselines/getting_started/aws/aws_baseline/aws_service_enable.tf
+++ baselines/getting_started/aws/aws_baseline/aws_service_enable.tf
@@ -1,11 +1,6 @@
 # Enable all AWS Services within the Variables file
 # More Info: https://turbot.com/v5/docs/integrations/aws/services
 
-variable "enabled_policy_map" {
-  description = "Enter the list of services that you would like to Enable"
-  type        = map
-}
-
 #Loop through var.service_status and set enable policies
 resource "turbot_policy_setting" "aws_enable" {
   for_each = var.enabled_policy_map
