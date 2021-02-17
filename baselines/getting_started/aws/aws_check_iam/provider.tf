8d68c87774a50548c5c2adcbff2120f6a4d1d5d4
debabrat-git <43408569+debabrat-git@users.noreply.github.com>
2021-02-17 19:21:07 +0530

    Baseline - Refactor AWS check IAM. Closes #368 (#404)

--- baselines/getting_started/aws/aws_check_iam/provider.tf
+++ baselines/getting_started/aws/aws_check_iam/provider.tf
@@ -6,3 +6,8 @@ terraform {
   }
   required_version = ">= 0.13"
 }
+
+provider "turbot" {
+  profile = var.turbot_profile
+}
+
--- baselines/getting_started/aws/aws_check_iam/role_policy_attachment.tf
+++ baselines/getting_started/aws/aws_check_iam/role_policy_attachment.tf
@@ -1,18 +1,24 @@
 # Check unapproved IAM Role policy attachments based on name (e.g. FullAccess)
 
 # Set policy to check unapproved policy attachments
-resource "turbot_policy_setting" "aws_iam_role_policy_attachement_approved" {
-    resource        = turbot_smart_folder.aws_iam.id
-    type            = "tmod:@turbot/aws-iam#/policy/types/rolePolicyAttachmentsApproved"
-    value           = "Check: Approved"
-                      ## "Enforce: Delete unapproved"
+# AWS > IAM > Role > Policy Attachments > Approved
+# https://turbot.com/v5/mods/turbot/aws-iam/inspect#/policy/types/rolePolicyAttachmentsApproved
+resource "turbot_policy_setting" "iam_role_policy_attachement_approved" {
+  count    = var.enable_iam_role_policy_attachement_approved ? 1 : 0
+  resource = turbot_smart_folder.aws_iam.id
+  type     = "tmod:@turbot/aws-iam#/policy/types/rolePolicyAttachmentsApproved"
+  value    = "Check: Approved"
+  ## "Enforce: Delete unapproved"
 }
 
 # Identify policy names that are unapproved
-resource "turbot_policy_setting" "aws_iam_role_policy_attachement_rules" {
-    resource        = turbot_smart_folder.aws_iam.id
-    type            = "tmod:@turbot/aws-iam#/policy/types/rolePolicyAttachmentsApprovedRules"
-    value           = <<-POLICY
+# AWS > IAM > Role > Policy Attachments > Approved > Rules
+# https://turbot.com/v5/mods/turbot/aws-iam/inspect#/policy/types/rolePolicyAttachmentsApprovedRules
+resource "turbot_policy_setting" "iam_role_policy_attachement_rules" {
+  count    = var.enable_iam_role_policy_attachement_rules ? 1 : 0
+  resource = turbot_smart_folder.aws_iam.id
+  type     = "tmod:@turbot/aws-iam#/policy/types/rolePolicyAttachmentsApprovedRules"
+  value    = <<-POLICY
         REJECT $.PolicyName:/^.+FullAccess.*$/
         REJECT $.PolicyName:AdministratorAccess
         APPROVE *
