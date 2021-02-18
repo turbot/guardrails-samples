60b44df51b938f93f89625e4cf44c96af46ba89d
raj <raj@turbot.com>
2021-02-19 00:36:47 +0530

    Reformatted code.

--- baselines/getting_started/azure/azure_check_public_access/applicationgateway_policies.tf
+++ baselines/getting_started/azure/azure_check_public_access/applicationgateway_policies.tf
@@ -1,22 +1,23 @@
 # Azure > Application Gateway Service > Application Gateway > Approved > Usage
 # https://turbot.com/v5/mods/turbot/azure-applicationgateway/inspect#/policy/types/applicationGatewayApprovedUsage
 resource "turbot_policy_setting" "azure_applicationgateway_application_gateway_approved_usage" {
-  count    = var.enable_application_gateway_approved_policies ? 1 : 0
+  count          = var.enable_application_gateway_approved_policies ? 1 : 0
   resource       = turbot_smart_folder.azure_public_access.id
   type           = "tmod:@turbot/azure-applicationgateway#/policy/types/applicationGatewayApprovedUsage"
   template_input = <<EOT
-{
-  applicationGateway{
-    frontendIPConfigurations
+  {
+    applicationGateway{
+      frontendIPConfigurations
+    }
   }
-}
-EOT
+  EOT
   template       = <<EOT
-{% set regExp = r/Succeeded/i %}
-{%- if regExp.test($.applicationGateway.frontendIPConfigurations[0].provisioningState) -%}
-"Not approved"
-{%- else -%}
-"Approved"
-{%- endif -%}
-EOT
+  {%- set result = "Approved" -%}
+  {%- set regExp = r/Succeeded/i -%}
+  {%- if regExp.test($.applicationGateway.frontendIPConfigurations[0].provisioningState) -%}
+    {%- set result = "Not approved" -%}
+  {%- endif -%}
+
+  {{ result }}
+  EOT
 }
