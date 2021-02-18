0da2a4a9a57484305390295d50e213f7e8359039
raj <raj@turbot.com>
2021-02-18 13:24:55 +0530

    Adjustment to output, default variable & readme.

--- baselines/getting_started/aws/aws_check_s3/outputs.tf
+++ baselines/getting_started/aws/aws_check_s3/outputs.tf
@@ -1,3 +1,47 @@
+output "trusted_accounts" {
+  value = var.trusted_accounts
+}
+
+output "enable_s3_access_logging_policies" {
+  value = var.enable_s3_access_logging_policies
+}
+
+output "enable_s3_active_policies" {
+  value = var.enable_s3_active_policies
+}
+
+output "enable_s3_approved_policies" {
+  value = var.enable_s3_approved_policies
+}
+
+output "enable_s3_enabled_policies" {
+  value = var.enable_s3_enabled_policies
+}
+
+output "enable_s3_encryption_policies" {
+  value = var.enable_s3_encryption_policies
+}
+
+output "enable_s3_permission_policies" {
+  value = var.enable_s3_permission_policies
+}
+
+output "enable_s3_public_access_policies" {
+  value = var.enable_s3_public_access_policies
+}
+
+output "enable_s3_tag_policies" {
+  value = var.enable_s3_tag_policies
+}
+
+output "enable_s3_trusted_access_policies" {
+  value = var.enable_s3_trusted_access_policies
+}
+
+output "use_simple_s3_bucket_versioning" {
+  value = var.use_simple_s3_bucket_versioning
+}
+
 output "turbot_profile" {
   value = var.turbot_profile
 }
