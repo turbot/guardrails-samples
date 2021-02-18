0dddcc46b1cec0648520a3463c8e03140f88c6ec
Alex Smith <fatalbert3390@gmail.com>
2021-02-18 08:21:39 -0500

    fixed global region issue

--- trimbot/trimbot_modules/lam_function.py
+++ trimbot/trimbot_modules/lam_function.py
@@ -22,29 +22,23 @@ class LambdaFunction(Resource):
         else:
             self.logger.info(f"Would remove function {self.function_name}")
 
-
 class LambdaFunctionResourceService(ResourceService):
     def __init__(self, session, recipe) -> None:
-        self.function_names = self.create_function_names(session)
         super().__init__(session, recipe, "lambda", "function")
-        
-    def create_function_names(self, session):
-
-        lambda_client = session.create_client("lambda", session.get_default_region())
+    
+    def get_all(self, region):
+        lambda_client = self.session.create_client("lambda", region)
         paginator = lambda_client.get_paginator('list_functions')
         response_iterator = paginator.paginate()
-
+        
         results = []
         for response in response_iterator:
             results += response.get('Functions')
-
-        return [function['FunctionName'] for function in results]
-
-
-    def get_all(self, region):
         
+        function_names = [function['FunctionName'] for function in results]
+    
         resources = []
-        for function_name in self.function_names:
+        for function_name in function_names:
             for filter in self.recipe["filters"]:
                 if filter["type"] == "startswith":
                     for value in filter["value"]:
