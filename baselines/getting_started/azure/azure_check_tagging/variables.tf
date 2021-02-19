f6635b0b0f82c9e19365bd76a130bb7173361a61
Krishna Kumar Pathman <52328522+krishna5891@users.noreply.github.com>
2021-02-19 20:51:11 +0530

    Baseline - Refactor Azure check tagging. Closes #382 (#426)

--- baselines/getting_started/azure/azure_check_tagging/variables.tf
+++ baselines/getting_started/azure/azure_check_tagging/variables.tf
@@ -1,5 +1,5 @@
 variable "resource_tags" {
-description = <<DESC
+  description = <<DESC
 List of services and resources to be checked if its Tags are correct.
 These tags must exist for Missing Tag use case if enabled
 required_tags = [
@@ -16,45 +16,57 @@ Acceptable Values:
   "Enforce: Set tags"
 Check demo.tfvars for an example of how to set this value.
 DESC
-type          =map(string)
-default = {
-    azure-aks-managed-cluster                       = "Check: Tags are correct"
-    # azure-apimanagement-service                     = "Check: Tags are correct"
-    # azure-application-gateway                       = "Check: Tags are correct"
-    # azure-application-insights-insight              = "Check: Tags are correct"
-    # azure-compute-availability-set                  = "Check: Tags are correct"
-    # azure-compute-disk                              = "Check: Tags are correct"
-    # azure-compute-disk-encryption-set               = "Check: Tags are correct"
-    # azure-compute-image                             = "Check: Tags are correct"
-    # azure-compute-snapshot                          = "Check: Tags are correct"
-    azure-compute-virtual-machine                   = "Check: Tags are correct"
-    azure-cosmosdb-database                         = "Check: Tags are correct"
-    # azure-databricks-workspace                      = "Check: Tags are correct"
-    # azure-datafactory-factory                       = "Check: Tags are correct"
-    # azure-dns-record-set                            = "Check: Tags are correct"
-    # azure-dns-zone                                  = "Check: Tags are correct"
-    # azure-firewall                                  = "Check: Tags are correct"
-    # azure-keyvault-vault                            = "Check: Tags are correct"
-    # azure-loadbalancer                              = "Check: Tags are correct"
-    azure-mysql-server                              = "Check: Tags are correct"
-    azure-network-application-security-group        = "Check: Tags are correct"
-    # azure-network-network-interface                 = "Check: Tags are correct"
-    # azure-network-network-security-groups           = "Check: Tags are correct"
-    # azure-network-public-ip-address                 = "Check: Tags are correct"
-    # azure-network-route-table                       = "Check: Tags are correct"
-    # azure-network-virtual-network                   = "Check: Tags are correct"
-    # azure-networkwatcher                            = "Check: Tags are correct"
-    azure-postgresql-server                         = "Check: Tags are correct"
-    # azure-recoveryservice-vault                     = "Check: Tags are correct"
-    azure-resourcegroup                             = "Check: Tags are correct"
-    # azure-searchmanagement-search-service           = "Check: Tags are correct"
-    azure-sql-database                              = "Check: Tags are correct"
-    # azure-sql-elastic-pool                          = "Check: Tags are correct"
-    azure-sql-server                                = "Check: Tags are correct"
-    azure-storage-storage-account                   = "Check: Tags are correct"
-    # azure-synapseanalytics-sql-pool                 = "Check: Tags are correct"
-    azure-synapseanalytics-workspace                = "Check: Tags are correct"
+  type        = map(string)
+  default = {
+    azure-aks-managed-cluster = "Check: Tags are correct"
+    # azure-apimanagement-service = "Check: Tags are correct"
+    # azure-application-gateway = "Check: Tags are correct"
+    # azure-application-insights-insight = "Check: Tags are correct"
+    # azure-compute-availability-set = "Check: Tags are correct"
+    # azure-compute-disk = "Check: Tags are correct"
+    # azure-compute-disk-encryption-set = "Check: Tags are correct"
+    # azure-compute-image = "Check: Tags are correct"
+    # azure-compute-snapshot = "Check: Tags are correct"
+    azure-compute-virtual-machine = "Check: Tags are correct"
+    azure-cosmosdb-database       = "Check: Tags are correct"
+    # azure-databricks-workspace = "Check: Tags are correct"
+    # azure-datafactory-factory = "Check: Tags are correct"
+    # azure-dns-record-set = "Check: Tags are correct"
+    # azure-dns-zone = "Check: Tags are correct"
+    # azure-firewall = "Check: Tags are correct"
+    # azure-keyvault-vault = "Check: Tags are correct"
+    # azure-loadbalancer = "Check: Tags are correct"
+    azure-mysql-server                       = "Check: Tags are correct"
+    azure-network-application-security-group = "Check: Tags are correct"
+    # azure-network-network-interface = "Check: Tags are correct"
+    # azure-network-network-security-groups = "Check: Tags are correct"
+    # azure-network-public-ip-address = "Check: Tags are correct"
+    # azure-network-route-table = "Check: Tags are correct"
+    # azure-network-virtual-network = "Check: Tags are correct"
+    # azure-networkwatcher = "Check: Tags are correct"
+    azure-postgresql-server = "Check: Tags are correct"
+    # azure-recoveryservice-vault = "Check: Tags are correct"
+    azure-resourcegroup = "Check: Tags are correct"
+    # azure-searchmanagement-search-service = "Check: Tags are correct"
+    azure-sql-database = "Check: Tags are correct"
+    # azure-sql-elastic-pool = "Check: Tags are correct"
+    azure-sql-server              = "Check: Tags are correct"
+    azure-storage-storage-account = "Check: Tags are correct"
+    # azure-synapseanalytics-sql-pool = "Check: Tags are correct"
+    azure-synapseanalytics-workspace = "Check: Tags are correct"
   }
+}
+
+variable "set_resource_tag_policies" {
+  type        = bool
+  description = "Set resource tag policies for baseline"
+  default     = true
+}
+
+variable "default_tag_template" {
+  type        = bool
+  description = "Set defualt tag template policies for baseline"
+  default     = true
 }
 
 variable "turbot_profile" {
@@ -77,4 +89,4 @@ variable "smart_folder_parent_resource" {
   description = "Enter the resource ID or AKA for the parent of the smart folder"
   type        = string
   default     = "tmod:@turbot/turbot#/"
-} 
+}
