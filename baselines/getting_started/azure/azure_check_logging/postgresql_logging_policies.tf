e58dd0ccc5dd1d5e7abe7307381c688ff09d7cab
Krishna Kumar Pathman <52328522+krishna5891@users.noreply.github.com>
2021-02-19 18:41:55 +0530

    Baseline - Refactor Azure check logging. Closes #378 (#427)

--- baselines/getting_started/azure/azure_check_logging/postgresql_logging_policies.tf
+++ baselines/getting_started/azure/azure_check_logging/postgresql_logging_policies.tf
@@ -1,63 +1,52 @@
-## Azure SQL Server
-resource "turbot_policy_setting" "azure_sql_server_auditing" {
-  resource = turbot_smart_folder.azure_logging.id
-  type     = "tmod:@turbot/azure-sql#/policy/types/serverAuditing"
-  value    = "Check: Enabled"
-}
-
-resource "turbot_policy_setting" "azure_sql_server_data_security" {
-  resource = turbot_smart_folder.azure_logging.id
-  type     = "tmod:@turbot/azure-sql#/policy/types/serverDataSecurity"
-  value    = "Check: Enabled"
-}
-
-
-## Azure SQL Database
-resource "turbot_policy_setting" "azure_sql_database_auditing" {
-  resource = turbot_smart_folder.azure_logging.id
-  type     = "tmod:@turbot/azure-sql#/policy/types/databaseAuditing"
-  value    = "Check: Enabled"
-}
-
-resource "turbot_policy_setting" "azure_sql_database_data_security" {
-  resource = turbot_smart_folder.azure_logging.id
-  type     = "tmod:@turbot/azure-sql#/policy/types/databaseDataSecurity"
-  value    = "Check: Enabled"
-}
-
-
-## Azure PostgrSQL
+# Azure > PostgreSQL > Server > Audit Logging
+# https://turbot.com/v5/mods/turbot/azure-postgresql/inspect#/policy/types/serverAuditLogging
 resource "turbot_policy_setting" "azure_postgresql_server_auditing" {
+  count    = var.azure_postgresql_server_auditing_polices ? 1 : 0
   resource = turbot_smart_folder.azure_logging.id
   type     = "tmod:@turbot/azure-postgresql#/policy/types/serverAuditLogging"
   value    = "Check: Audit Logging > *"
 }
 
+# Azure > PostgreSQL > Server > Audit Logging > Log Checkpoints
+# https://turbot.com/v5/mods/turbot/azure-postgresql/inspect#/policy/types/serverAuditLoggingLogCheckpoints
 resource "turbot_policy_setting" "azure_postgresql_server_auditing_checkpoints" {
+  count    = var.azure_postgresql_server_auditing_checkpoints_polices ? 1 : 0
   resource = turbot_smart_folder.azure_logging.id
   type     = "tmod:@turbot/azure-postgresql#/policy/types/serverAuditLoggingLogCheckpoints"
   value    = "On"
 }
 
+# Azure > PostgreSQL > Server > Audit Logging > Log Connections
+# https://turbot.com/v5/mods/turbot/azure-postgresql/inspect#/policy/types/serverAuditLoggingLogConnections
 resource "turbot_policy_setting" "azure_postgresql_server_auditing_connections" {
+  count    = var.azure_postgresql_server_auditing_connections_polices ? 1 : 0
   resource = turbot_smart_folder.azure_logging.id
   type     = "tmod:@turbot/azure-postgresql#/policy/types/serverAuditLoggingLogConnections"
   value    = "On"
 }
 
+# Azure > PostgreSQL > Server > Audit Logging > Log Disconnections
+# https://turbot.com/v5/mods/turbot/azure-postgresql/inspect#/policy/types/serverAuditLoggingLogDisconnections
 resource "turbot_policy_setting" "azure_postgresql_server_auditing_disconnections" {
+  count    = var.azure_postgresql_server_auditing_disconnections_polices ? 1 : 0
   resource = turbot_smart_folder.azure_logging.id
   type     = "tmod:@turbot/azure-postgresql#/policy/types/serverAuditLoggingLogDisconnections"
   value    = "On"
 }
 
+# Azure > PostgreSQL > Server > Audit Logging > Log Duration
+# https://turbot.com/v5/mods/turbot/azure-postgresql/inspect#/policy/types/serverAuditLoggingLogDuration
 resource "turbot_policy_setting" "azure_postgresql_server_auditing_duration" {
+  count    = var.azure_postgresql_server_auditing_duration_polices ? 1 : 0
   resource = turbot_smart_folder.azure_logging.id
   type     = "tmod:@turbot/azure-postgresql#/policy/types/serverAuditLoggingLogDuration"
   value    = "On"
 }
 
+# Azure > PostgreSQL > Server > Audit Logging > Log Retention Days
+# https://turbot.com/v5/mods/turbot/azure-postgresql/inspect#/policy/types/serverAuditLoggingLogRetentionDays
 resource "turbot_policy_setting" "azure_postgresql_server_auditing_duration_days" {
+  count    = var.azure_postgresql_server_auditing_duration_days_polices ? 1 : 0
   resource = turbot_smart_folder.azure_logging.id
   type     = "tmod:@turbot/azure-postgresql#/policy/types/serverAuditLoggingLogRetentionDays"
   value    = ">= 1 Day"
