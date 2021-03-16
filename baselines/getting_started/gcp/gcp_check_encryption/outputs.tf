a21062b5c908bb2a9385fa85d2508873b7e54bfb
omero <omero@turbot.com>
2021-03-16 14:36:11 +0000

    Refactored to be consistent

--- baselines/getting_started/gcp/gcp_check_encryption/outputs.tf
+++ baselines/getting_started/gcp/gcp_check_encryption/outputs.tf
@@ -18,66 +18,50 @@ output "enable_bigquery_dataset_approved_policies" {
   value = var.enable_bigquery_dataset_approved_policies
 }
 
-output "enable_bigquery_dataset_approved_encryption_at_rest_policies" {
-  value = var.enable_bigquery_dataset_approved_encryption_at_rest_policies
-}
-
-output "enable_bigquery_table_approved_policies" {
-  value = var.enable_bigquery_table_approved_policies
-}
-
-output "enable_bigquery_table_approved_encryption_at_rest_policies" {
-  value = var.enable_bigquery_table_approved_encryption_at_rest_policies
+output "enable_bigquery_encryption_policies" {
+  value = var.enable_bigquery_encryption_policies
 }
 
 output "enable_computeengine_image_approved_policies" {
   value = var.enable_computeengine_image_approved_policies
 }
 
-output "enable_computeengine_image_approved_encryption_at_rest_policies" {
-  value = var.enable_computeengine_image_approved_encryption_at_rest_policies
+output "enable_compute_engine_encryption_policies" {
+  value = var.enable_compute_engine_encryption_policies
 }
 
 output "enable_dataflow_job_approved_policies" {
   value = var.enable_dataflow_job_approved_policies
 }
 
-output "enable_dataflow_job_approved_encryption_at_rest_policies" {
-  value = var.enable_dataflow_job_approved_encryption_at_rest_policies
+output "enable_dataflow_encryption_policies" {
+  value = var.enable_dataflow_encryption_policies
 }
 
 output "enable_dataproc_cluster_approved_policies" {
   value = var.enable_dataproc_cluster_approved_policies
 }
 
-output "enable_dataproc_cluster_approved_encryption_at_rest_policies" {
-  value = var.enable_dataproc_cluster_approved_encryption_at_rest_policies
+output "enable_dataproc_encryption_policies" {
+  value = var.enable_dataproc_encryption_policies
 }
 
 output "enable_kubernetesengine_region_cluster_approved_policies" {
   value = var.enable_kubernetesengine_region_cluster_approved_policies
 }
 
-output "enable_kubernetesengine_region_cluster_approved_encryption_at_rest_policies" {
-  value = var.enable_kubernetesengine_region_cluster_approved_encryption_at_rest_policies
-}
-
-output "enable_kubernetesengine_zone_cluster_approved_policies" {
-  value = var.enable_kubernetesengine_zone_cluster_approved_policies
-}
-
-output "enable_kubernetesengine_zone_cluster_approved_encryption_at_rest_policies" {
-  value = var.enable_kubernetesengine_zone_cluster_approved_encryption_at_rest_policies
+output "enable_kubernetes_engine_encryption_policies" {
+  value = var.enable_kubernetes_engine_encryption_policies
 }
 
 output "enable_pubsub_topic_approved_policies" {
   value = var.enable_pubsub_topic_approved_policies
 }
 
-output "enable_pubsub_topic_approved_encryption_at_rest_policies" {
-  value = var.enable_pubsub_topic_approved_encryption_at_rest_policies
+output "enable_pub_sub_encryption_policies" {
+  value = var.enable_pub_sub_encryption_policies
 }
 
-output "enable_storage_bucket_encryption_at_rest_policies" {
-  value = var.enable_storage_bucket_encryption_at_rest_policies
+output "enable_storage_encryption_policies" {
+  value = var.enable_storage_encryption_policies
 }
