83ceaedf1f32ef953a8518b0d96a414c49696762
Omero Saienni <39497847+omerosaienni@users.noreply.github.com>
2021-03-16 14:49:10 +0000

    Baseline - Simplify GCP check encryption and address consistency #498 (#499)

--- baselines/getting_started/gcp/gcp_check_encryption/dataproc_encryption_policies.tf
+++ baselines/getting_started/gcp/gcp_check_encryption/dataproc_encryption_policies.tf
@@ -3,23 +3,23 @@
 # GCP > Dataproc > Cluster > Approved
 # https://turbot.com/v5/mods/turbot/gcp-dataproc/inspect#/policy/types/clusterApproved
 resource "turbot_policy_setting" "gcp_dataproc_cluster_approved" {
-  count    = var.enable_dataproc_cluster_approved_policies ? 1 : 0
+  count    = var.enable_dataproc_encryption_policies ? 1 : 0
   resource = turbot_smart_folder.gcp_encryption.id
   type     = "tmod:@turbot/gcp-dataproc#/policy/types/clusterApproved"
   value    = "Check: Approved"
-          # "Enforce: Delete unapproved if new"
+  # "Enforce: Delete unapproved if new"
 }
 
 # GCP > Dataproc > Cluster > Approved > Encryption at Rest
 # https://turbot.com/v5/mods/turbot/gcp-dataproc/inspect#/policy/types/clusterApprovedEncryptionAtRest
 resource "turbot_policy_setting" "gcp_dataproc_cluster_approved_encryption_at_rest" {
-  count    = var.enable_dataproc_cluster_approved_encryption_at_rest_policies ? 1 : 0
+  count    = var.enable_dataproc_encryption_policies ? 1 : 0
   resource = turbot_smart_folder.gcp_encryption.id
   type     = "tmod:@turbot/gcp-dataproc#/policy/types/clusterApprovedEncryptionAtRest"
   value    = "Google managed key"
-          # "Google managed key"
-          # "Google managed key or higher"
-          # "Customer managed key"
-          # "Customer managed key or higher"
-          # "Encryption at Rest > Customer Managed Key"
+  # "Google managed key"
+  # "Google managed key or higher"
+  # "Customer managed key"
+  # "Customer managed key or higher"
+  # "Encryption at Rest > Customer Managed Key"
 }
--- baselines/getting_started/gcp/gcp_check_encryption/storage_encryption_policies.tf
+++ baselines/getting_started/gcp/gcp_check_encryption/storage_encryption_policies.tf
@@ -3,17 +3,17 @@
 # GCP > Storage > Bucket > Encryption at Rest
 # https://turbot.com/v5/mods/turbot/gcp-storage/inspect#/policy/types/bucketEncryptionAtRest
 resource "turbot_policy_setting" "gcp_storage_bucket_encryption_at_rest" {
-  count    = var.enable_storage_bucket_encryption_at_rest_policies ? 1 : 0
+  count    = var.enable_storage_encryption_policies ? 1 : 0
   resource = turbot_smart_folder.gcp_encryption.id
   type     = "tmod:@turbot/gcp-storage#/policy/types/bucketEncryptionAtRest"
   value    = "Check: Google managed key"
-          # "Skip",
-          # "Check: Google managed key",
-          # "Check: Google managed key or higher",
-          # "Check: Customer managed key",
-          # "Check: Encryption at Rest > Customer Managed Key",
-          # "Enforce: Google managed key",
-          # "Enforce: Google managed key or higher",
-          # "Enforce: Customer managed key",
-          # "Enforce: Encryption at Rest > Customer Managed Key"
+  # "Skip",
+  # "Check: Google managed key",
+  # "Check: Google managed key or higher",
+  # "Check: Customer managed key",
+  # "Check: Encryption at Rest > Customer Managed Key",
+  # "Enforce: Google managed key",
+  # "Enforce: Google managed key or higher",
+  # "Enforce: Customer managed key",
+  # "Enforce: Encryption at Rest > Customer Managed Key"
 }
