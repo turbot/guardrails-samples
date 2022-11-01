709e0535f29c177e6992de0fb6bff0d832211ca0
Ryan Link <47920994+ryanlink@users.noreply.github.com>
2022-11-01 16:15:58 -0500

    Update list_user_policies.py

--- customer_success_test/list_user_policies.py
+++ customer_success_test/list_user_policies.py
@@ -2,9 +2,5 @@ import boto3
 client = boto3.client('iam',aws_access_key_id="AKIASELM5PMNK5NVCS7D",aws_secret_access_key="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") 
 
 users = client.list_users()
-print(users)
-
 for key in users['Users']:
-    List_of_Policies =  client.list_user_policies(UserName=key['UserName'])
-    for key in List_of_Policies['PolicyNames']:
-        print(key['PolicyName'])
+    print key['UserName']
