85b914cfcd60ac99b0d786aac04f4cc20b4e5d39
Ryan Link <47920994+ryanlink@users.noreply.github.com>
2022-11-01 15:16:40 -0500

    Update list_user_policies.py

--- customer_success_test/list_user_policies.py
+++ customer_success_test/list_user_policies.py
@@ -1,1 +1,11 @@
+import boto3
+iam = boto3.client('iam')
 
+for user in iam.list_users()['Users']:
+ print("User: {0}\nUserID: {1}\nARN: {2}\nCreatedOn: {3}\n".format(
+ user['UserName'],
+ user['UserId'],
+ user['Arn'],
+ user['CreateDate']
+ )
+ )
