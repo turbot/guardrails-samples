c0155d33db198572bea5450f437f30e1450c844a
David Boeke <david@boeke.com>
2024-05-08 07:41:39 -0400

    section 2 start

--- cis_enforcements/aws/cis_v300_s2_storage/enforce_efs.tf
+++ cis_enforcements/aws/cis_v300_s2_storage/enforce_efs.tf
@@ -1,7 +1,23 @@
-# # AWS > EFS > Ensure Encryption
-# resource "turbot_policy_setting" "aws_efs_encryption" {
-#   resource = turbot_smart_folder.aws_cis_v300_s2_storage.id
-#   type     = "tmod:@turbot/aws-efs#/policy/types/encryption"
-#   value    = "Enforce: Enabled"
-#   note     = "AWS CIS v3.0.0 - Control: 2.4.1"
-# }
+# AWS > EFS > FileSystem > Approved
+resource "turbot_policy_setting" "aws_efs_file_system_approved" {
+  resource = turbot_smart_folder.aws_cis_v_300_s_2_storage.id
+  type     = "tmod:@turbot/aws-efs#/policy/types/fileSystemApproved"
+  note     = "AWS CIS v3.0.0 - Control: 2.4.1"
+  value    = "Check: Approved"
+}
+
+# AWS > EFS > FileSystem > Approved > Usage
+resource "turbot_policy_setting" "aws_efs_file_system_approved_usage" {
+  resource = turbot_smart_folder.aws_cis_v_300_s_2_storage.id
+  type     = "tmod:@turbot/aws-efs#/policy/types/fileSystemApprovedUsage"
+  note     = "AWS CIS v3.0.0 - Control: 2.4.1"
+  value    = "Approved"
+}
+
+# AWS > EFS > FileSystem > Approved > Encryption at Rest
+resource "turbot_policy_setting" "aws_efs_file_system_encryption_at_rest" {
+  resource = turbot_smart_folder.aws_cis_v_300_s_2_storage.id
+  type     = "tmod:@turbot/aws-efs#/policy/types/fileSystemEncryptionAtRest"
+  note     = "AWS CIS v3.0.0 - Control: 2.4.1"
+  value    = "AWS managed key or higher"
+}
--- cis_enforcements/aws/cis_v300_s2_storage/enforce_rds.tf
+++ cis_enforcements/aws/cis_v300_s2_storage/enforce_rds.tf
@@ -1,23 +1,40 @@
-# # AWS > RDS > Ensure Encryption-at-Rest
-# resource "turbot_policy_setting" "aws_rds_encryption_at_rest" {
-#   resource = turbot_smart_folder.aws_cis_v300_s2_storage.id
-#   type     = "tmod:@turbot/aws-rds#/policy/types/instanceEncryption"
-#   value    = "Enforce: Enabled"
-#   note     = "AWS CIS v3.0.0 - Control: 2.3.1"
-# }
+# AWS > RDS > DB Instance > Approved
+resource "turbot_policy_setting" "aws_rds_db_instance_approved" {
+  resource = turbot_smart_folder.aws_cis_v_300_s_2_storage.id
+  type     = "tmod:@turbot/aws-rds#/policy/types/dbInstanceApproved"
+  note     = "AWS CIS v3.0.0 - Control: 2.3.1"
+  value    = "Check: Approved"
+}
 
-# # AWS > RDS > Auto Minor Version Upgrade
-# resource "turbot_policy_setting" "aws_rds_auto_minor_version_upgrade" {
-#   resource = turbot_smart_folder.aws_cis_v300_s2_storage.id
-#   type     = "tmod:@turbot/aws-rds#/policy/types/autoMinorVersionUpgrade"
-#   value    = "Enforce: Enabled"
-#   note     = "AWS CIS v3.0.0 - Control: 2.3.2"
-# }
+# AWS > RDS > DB Instance > Approved > Usage
+resource "turbot_policy_setting" "aws_rds_db_instance_approved_usage" {
+  resource = turbot_smart_folder.aws_cis_v_300_s_2_storage.id
+  type     = "tmod:@turbot/aws-rds#/policy/types/dbInstanceApprovedUsage"
+  note     = "AWS CIS v3.0.0 - Control: 2.3.1"
+  value    = "Approved"
+}
 
-# # AWS > RDS > Deny Public Access
-# resource "turbot_policy_setting" "aws_rds_deny_public_access" {
-#   resource = turbot_smart_folder.aws_cis_v300_s2_storage.id
-#   type     = "tmod:@turbot/aws-rds#/policy/types/publicAccess"
-#   value    = "Enforce: Denied"
-#   note     = "AWS CIS v3.0.0 - Control: 2.3.3"
-# }
+# AWS > RDS > DB Instance > Approved > Encryption at Rest
+resource "turbot_policy_setting" "aws_rds_db_instance_encryption_at_rest" {
+  resource = turbot_smart_folder.aws_cis_v_300_s_2_storage.id
+  type     = "tmod:@turbot/aws-rds#/policy/types/dbInstanceEncryptionAtRest"
+  note     = "AWS CIS v3.0.0 - Control: 2.3.1"
+  value    = "AWS managed key or higher"
+}
+
+# AWS > RDS > DB Instance > Auto Minor Version Upgrade
+resource "turbot_policy_setting" "aws_rds_db_instance_auto_minor_version_upgrade" {
+  resource = turbot_smart_folder.aws_cis_v_300_s_2_storage.id
+  type     = "tmod:@turbot/aws-rds#/policy/types/dbInstanceAutoMinorVersionUpgrade"
+  note     = "AWS CIS v3.0.0 - Control: 2.3.2"
+  value    = "Check: Enabled"
+}
+
+# AWS > RDS > DB Instance > Publicly Accessible
+resource "turbot_policy_setting" "aws_rds_db_instance_publicly_accessible" {
+  resource = turbot_smart_folder.aws_cis_v_300_s_2_storage.id
+  type     = "tmod:@turbot/aws-rds#/policy/types/dbInstancePubliclyAccessible"
+  note     = "AWS CIS v3.0.0 - Control: 2.3.3"
+  value    = "Check: DB Instance is not publicly accessible"
+}
+
