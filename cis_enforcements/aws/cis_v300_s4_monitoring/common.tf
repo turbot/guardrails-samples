935bf9b6a7c1a28a7df5021c903ad2c3e2308074
Sumit Popat <sumit@turbot.com>
2024-05-14 21:15:05 +0530

    Add samples for aws cis 300 section 4.

---
+++ cis_enforcements/aws/cis_v300_s4_monitoring/common.tf
@@ -0,0 +1,34 @@
+# Smart Folder
+resource "turbot_smart_folder" "aws_cis_v300_s4_monitoring" {
+  parent      = "tmod:@turbot/turbot#/"
+  title       = "AWS CIS v3.0.0 - Section 4 - Monitoring"
+  description = "This section contains recommendations for configuring AWS monitoring features."
+}
+
+# AWS > CloudTrail > Enabled
+resource "turbot_policy_setting" "aws_cloudtrail_enabled" {
+  resource = turbot_smart_folder.aws_cis_v300_s4_monitoring.id
+  type     = "tmod:@turbot/aws-cloudtrail#/policy/types/cloudTrailEnabled"
+  value    = "Enabled"
+}
+
+# AWS > CloudWatch > Enabled
+resource "turbot_policy_setting" "aws_cloudtrail_enabled" {
+  resource = turbot_smart_folder.aws_cis_v300_s4_monitoring.id
+  type     = "tmod:@turbot/aws-cloudwatch#/policy/types/cloudWatchEnabled"
+  value    = "Enabled"
+}
+
+# AWS > Logs > Enabled
+resource "turbot_policy_setting" "aws_logs_enabled" {
+  resource = turbot_smart_folder.aws_cis_v300_s4_monitoring.id
+  type     = "tmod:@turbot/aws-logs#/policy/types/logsEnabled"
+  value    = "Enabled"
+}
+
+# AWS > Security Hub > Enabled
+resource "turbot_policy_setting" "aws_security_hub_enabled" {
+  resource = turbot_smart_folder.aws_cis_v300_s4_monitoring.id
+  type     = "tmod:@turbot/aws-logs#/policy/types/securityHubEnabled"
+  value    = "Enabled"
+}
