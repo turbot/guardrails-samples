e16523fdd93b3274af8aa1e89497e0ad72db7fdc
niharika2k00 <dniharika16@gmail.com>
2024-06-01 00:14:17 +0530

    update

--- cis_enforcements/azure/cis_v200_s2_microsoft_defender/security_center.tf
+++ cis_enforcements/azure/cis_v200_s2_microsoft_defender/security_center.tf
@@ -14,21 +14,7 @@ resource "turbot_policy_setting" "azure_securitycenter_defender_plan_resource_ty
   resource = turbot_smart_folder.azure_cis_v200_s2_microsoft_defender.id
   type     = "tmod:@turbot/azure-securitycenter#/policy/types/securityCenterDefenderPlanResourceType"
   note     = "Azure CIS v2.0.0 - Controls: 2.1.1, 2.1.2, 2.1.3, 2.1.4, 2.1.5, 2.1.6, 2.1.7, 2.1.8, 2.1.9, 2.1.10, 2.1.11, 2.1.12"
-  value    = "Servers"
-  # value    = "API"
-  # value =  "App Service"
-  # value =  "Azure SQL Databases"
-  # value =  "Cloud Posture"
-  # value =  "Container registries"
-  # value =  "Containers"
-  # value =  "Cosmos DB"
-  # value =  "DNS"
-  # value =  "Key Vault"
-  # value =  "Kubernetes"
-  # value =  "Open-source relational databases"
-  # value =  "Resource Manager"
-  # value =  "SQL servers on machines"
-  # value =  "Storage"
+  value    = jsonencode(["Servers", "API", "App Service", "Azure SQL Databases", "Cloud Posture", "Container registries", "Containers", "Cosmos DB", "DNS", "Key Vault", "Kubernetes", "Open-source relational databases", "Resource Manager", "SQL servers on machines", "Storage"])
 }
 
 # Azure > Security Center > Security Center > Auto Provisioning
