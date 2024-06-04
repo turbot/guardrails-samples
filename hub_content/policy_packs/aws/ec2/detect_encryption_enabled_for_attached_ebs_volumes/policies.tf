4093cc233ab7c62df227aed6edd1e822415e3abb
niharika2k00 <dniharika16@gmail.com>
2024-06-04 16:22:55 +0530

    update.

--- hub_content/policy_packs/aws/ec2/detect_encryption_enabled_for_attached_ebs_volumes/policies.tf
+++ hub_content/policy_packs/aws/ec2/detect_encryption_enabled_for_attached_ebs_volumes/policies.tf
@@ -5,28 +5,25 @@ resource "turbot_policy_setting" "aws_ec2_volume_approved" {
   template_input = <<-EOT
       {
         volume {
-          Attachments: get(path: "Attachments")
-          Encrypted: get(path: "Encrypted")
+          attachments: get(path: "Attachments")
+          encrypted: get(path: "Encrypted")
         }
       }
       EOT
   template       = <<-EOT
-      {%- set attachedWithInstance = false -%}
-      {%- for attachment in $.volume.Attachments -%}
-      {%- if attachment.InstanceId -%}
-      {%- set attachedWithInstance = true -%}
-      {%- endif -%}
-      {%- endfor -%}
-
-      {#- If AWS EBS volume is attached with EC2 instance -#}
-      {%- if attachedWithInstance and $.volume.Encrypted -%}
-      Check: Approved
-      {#- Enforce: Detach unapproved if new -#}
-      {#- Enforce: Detach, snapshot and delete unapproved if new -#}
-      {%- else -%}
-      Skip
-      {%- endif -%}
-      EOT
+    {%- set attachedWithInstance = false -%}
+    {%- for attachment in $.volume.attachments -%}
+    {#- If AWS EBS volume is attached with EC2 instance -#}
+    {%- if attachment.InstanceId -%}
+    {%- set attachedWithInstance = true -%}
+    {%- if attachedWithInstance and $.volume.encrypted -%}
+    Check: Approved
+    {#- Enforce: Detach unapproved if new -#}
+    {#- Enforce: Detach, snapshot and delete unapproved if new -#}
+    {%- endif -%}
+    {%- endif -%}
+    {%- endfor -%}
+    EOT
 }
 
 # AWS > EC2 > Volume > Encryption at Rest
@@ -37,28 +34,24 @@ resource "turbot_policy_setting" "aws_ec2_volume_encryption_at_rest" {
   template_input = <<-EOT
     {
       volume {
-        Attachments: get(path: "Attachments")
-        Encrypted: get(path: "Encrypted")
+        attachments: get(path: "Attachments")
+        encrypted: get(path: "Encrypted")
       }
     }
     EOT
   template       = <<-EOT
     {%- set attachedWithInstance = false -%}
-    {%- for attachment in $.volume.Attachments -%}
+    {%- for attachment in $.volume.attachments -%}
+    {#- If AWS EBS volume is attached with EC2 instance -#}
     {%- if attachment.InstanceId -%}
     {%- set attachedWithInstance = true -%}
-    {%- endif -%}
-    {%- endfor -%}
-
-    {%- if attachedWithInstance and $.volume.Encrypted -%}
-    {#- AWS EBS volume is attached with EC2 instance -#}
+    {%- if attachedWithInstance and $.volume.encrypted -%}
     AWS managed key or higher
     {#- AWS managed key -#}
     {#- Customer managed key -#}
     {#- Encryption at Rest > Customer Managed Key -#}
-    {%- else -%}
-    {#- AWS EBS volume is NOT attached with EC2 instance -#}
-    None or higher
     {%- endif -%}
+    {%- endif -%}
+    {%- endfor -%}
     EOT
 }
