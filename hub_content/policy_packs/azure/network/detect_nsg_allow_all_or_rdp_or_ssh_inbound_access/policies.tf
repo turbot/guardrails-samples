363708593f886938bfafa2ba22c9334b9e285f0e
niharika2k00 <dniharika16@gmail.com>
2024-06-05 16:24:29 +0530

    update policy.

--- hub_content/policy_packs/azure/network/detect_nsg_allow_all_or_rdp_or_ssh_inbound_access/policies.tf
+++ hub_content/policy_packs/azure/network/detect_nsg_allow_all_or_rdp_or_ssh_inbound_access/policies.tf
@@ -6,15 +6,20 @@ resource "turbot_policy_setting" "azure_network_security_group_ingress_rules_app
   # value    = "Enforce: Delete unapproved"
 }
 
-# Azure > Network > Network Security Group > Custom
+# Azure > Network > Network Security Group > Ingress Rules > Approved > Custom
 resource "turbot_policy_setting" "azure_network_security_group_ingress_rules_approved_rules" {
   resource = turbot_smart_folder.pack.id
   type     = "tmod:@turbot/azure-network#/policy/types/networkSecurityGroupIngressRulesApprovedRules"
   template = <<-EOT
-    APPROVE $.turbot.fromPort:=3389 $.turbot.toPort:=3389
+    {#- Reject ports 22, 3389 -#}
+    REJECT $.turbot.fromPort:="22" $.turbot.toPort:="22"
 
-    APPROVE $.turbot.fromPort:=22 $.turbot.toPort:=22
+    REJECT $.turbot.fromPort:="3389" $.turbot.toPort:="3389"
 
+    {#- Reject any inbound from internet -#}
+    REJECT $.turbot.cidr:=0.0.0.0/0
+
+    {#- Approve unmatched rules -#}
     APPROVE *
     EOT
 }
