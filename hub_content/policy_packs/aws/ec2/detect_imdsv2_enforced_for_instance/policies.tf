50d1201d46005273723a9c1b1c42649fce9081ae
niharika2k00 <dniharika16@gmail.com>
2024-06-06 14:08:05 +0530

    update imdsv2 to use custom policy.

--- hub_content/policy_packs/aws/ec2/detect_imdsv2_enforced_for_instance/policies.tf
+++ hub_content/policy_packs/aws/ec2/detect_imdsv2_enforced_for_instance/policies.tf
@@ -1,33 +1,18 @@
-# AWS > EC2 > Instance > Approved
-resource "turbot_policy_setting" "aws_ec2_instance_approved" {
+# AWS > EC2 > Instance > Metadata Service
+resource "turbot_policy_setting" "aws_ec2_instance_metadata_service" {
   resource = turbot_smart_folder.pack.id
-  type     = "tmod:@turbot/aws-ec2#/policy/types/instanceApproved"
-  value    = "Check: Approved"
-  # value    = "Enforce: Delete unapproved if new"
-  # value    = "Enforce: Stop unapproved if new"
-  # value    = "Enforce: Stop unapproved"
+  type     = "tmod:@turbot/aws-ec2#/policy/types/instanceMetadataService"
+  value    = "Check: Enabled for V2 only"
+  # value =  "Check: Disabled"
+  # value =  "Check: Enabled for V1 and V2"
+  # value =  "Enforce: Disabled"
+  # value =  "Enforce: Enabled for V1 and V2"
+  # value =  "Enforce: Enabled for V2 only"
 }
 
-# AWS > EC2 > Instance > Approved > Custom
-resource "turbot_policy_setting" "aws_ec2_instance_approved_custom" {
-  resource       = turbot_smart_folder.pack.id
-  type           = "tmod:@turbot/aws-ec2#/policy/types/instanceApprovedCustom"
-  template_input = <<-EOT
-    {
-      instance {
-        httpTokens: get(path: "MetadataOptions.HttpTokens")
-      }
-    }
-    EOT
-  template       = <<-EOT
-    {%- if $.instance.httpTokens == "required" -%}
-      title: IMDSv2
-      result: Approved
-      message: "IMDSv2 is enforced for EC2 instance"
-    {%- else -%}
-      title: IMDSv2
-      result: Not approved
-      message: "IMDSv2 is not enforced for EC2 instance"
-    {%- endif -%}
-    EOT
+# AWS > EC2 > Instance > Metadata Service > HTTP Token Hop Limit
+resource "turbot_policy_setting" "aws_ec2_instance_metadata_service_token_hop_limit" {
+  resource = turbot_smart_folder.pack.id
+  type     = "tmod:@turbot/aws-ec2#/policy/types/instanceMetadataServiceTokenHopLimit"
+  value    = 1
 }
