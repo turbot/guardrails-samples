a3850831f87f5e2b57fd4643117e7d39083edfcd
Sumit Popat <sumit@turbot.com>
2024-06-10 18:20:11 +0530

    fix policy pack for azure app service web app runtime versions.

--- policy_packs/azure/appservice/enforce_webapp_do_not_use_outdated_java_php_python/policies.tf
+++ policy_packs/azure/appservice/enforce_webapp_do_not_use_outdated_java_php_python/policies.tf
@@ -12,6 +12,9 @@ resource "turbot_policy_setting" "azure_appservice_webapp_approved_custom" {
   type           = "tmod:@turbot/azure-appservice#/policy/types/webAppApprovedCustom"
   template_input = <<-EOT
     {
+      outdatedJavaVersions: constant(value: "['8', '11']")
+      outdatedPhpVersions: constant(value: "['5.6', '7.0', '7.1', '7.2', '7.3']")
+      outdatedPythonVersions: constant(value: "['2.7', '3.4', '3.5', '3.6']")
       webApp {
         javaVersion: get(path:"configuration.javaVersion"),
         phpVersion: get(path:"configuration.phpVersion"),
@@ -20,28 +23,28 @@ resource "turbot_policy_setting" "azure_appservice_webapp_approved_custom" {
     }
   EOT
   template       = <<-EOT
-    {%- if $.webApp.javaVersion in ['8', '11'] -%} 
+    {%- if $.webApp.javaVersion in $.outdatedJavaVersions -%} 
 
       {%- set data = { 
           "title": "Java Version",
           "result": "Not approved",
-          "message": "Web App is running on outdated Java version"
+          "message": "Web App is running with outdated Java version"
       } -%} 
 
-    {%- elif $.webApp.phpVersion in ['5.6', '7.0', '7.1', '7.2', '7.3'] -%} 
+    {%- elif $.webApp.phpVersion in $.outdatedPhpVersions -%} 
 
       {%- set data = { 
           "title": "PHP Version",
           "result": "Not approved",
-          "message": "Web App is running on outdated PHP version"
+          "message": "Web App is running with outdated PHP version"
       } -%} 
 
-    {%- elif $.webApp.pythonVersion in ['2.7', '3.4', '3.5', '3.6'] -%} 
+    {%- elif $.webApp.pythonVersion in $.outdatedPythonVersions -%} 
 
       {%- set data = { 
           "title": "Python Version",
           "result": "Not approved",
-          "message": "Web App is running on outdated Python version"
+          "message": "Web App is running with outdated Python version"
       } -%} 
 
     {%- else -%} 
@@ -75,7 +78,7 @@ resource "turbot_policy_setting" "azure_appservice_webapp_approved_custom" {
         {%- set data = { 
             "title": "Java/PHP/Python Version",
             "result": "Skip",
-            "message": "No data for Web App yet"
+            "message": "No data for web app yet"
         } -%} 
 
       {%- endif -%}
