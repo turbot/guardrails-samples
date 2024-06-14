55a10fc574a56d68318fcca9315851f50a5990cb
Sumit Popat <sumit@turbot.com>
2024-06-14 15:08:38 +0530

    fix casing.

--- policy_packs/gcp/network/enforce_firewall_rule_with_port_range_to_not_allow_incoming_traffic/common.tf
+++ policy_packs/gcp/network/enforce_firewall_rule_with_port_range_to_not_allow_incoming_traffic/common.tf
@@ -1,6 +1,6 @@
 # Policy Pack
 resource "turbot_smart_folder" "pack" {
-  title       = "Enforce GCP VPC Network Firewall Rules with Range of Ports to not allow incoming traffic"
+  title       = "Enforce GCP VPC Network Firewall Rules With Range Of Ports To Not Allow Incoming Traffic"
   description = "Delete GCP VPC network firewall rules with range of ports that are opened to allow incoming traffic."
   parent      = "tmod:@turbot/turbot#/"
 }
