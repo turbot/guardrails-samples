83ab7924a9745975944cb38fa44508ff6e18a1e5
sourav chakraborty <sourav@turbot.com>
2024-06-30 12:54:49 +0530

    update

--- cis_enforcements/azure/cis_v200_s7_vm/virtual_machine.tf
+++ cis_enforcements/azure/cis_v200_s7_vm/virtual_machine.tf
@@ -2,7 +2,7 @@
 resource "turbot_policy_setting" "azure_compute_virtual_machine_approved" {
   resource = turbot_smart_folder.azure_cis_v200_s7_virtual_machine.id
   type     = "tmod:@turbot/azure-compute#/policy/types/virtualMachineApproved"
-  note     = "Azure CIS v2.0.0 - Control: 7.2"
+  note     = "Azure CIS v2.0.0 - Control: 7.2 and 7.6"
   value    = "Check: Approved"
   # value    = "Enforce: Delete unapproved"
 }
@@ -11,21 +11,75 @@ resource "turbot_policy_setting" "azure_compute_virtual_machine_approved" {
 resource "turbot_policy_setting" "azure_compute_virtual_machine_approved_custom" {
   resource       = turbot_smart_folder.azure_cis_v200_s7_virtual_machine.id
   type           = "tmod:@turbot/azure-compute#/policy/types/virtualMachineApprovedCustom"
-  note           = "Azure CIS v2.0.0 - Control: 7.2"
+  note           = "Azure CIS v2.0.0 - Control: 7.2 and 7.6"
   template_input = <<-EOT
     {
+      installedExtensions: constant(value: "['MDE.Linux', 'extension2']")
       virtualMachine {
+        name
+        extensions: get(path: "resources")
         managedDiskId: get(path: "storageProfile.osDisk.managedDisk.id")
       }
     }
     EOT
   template       = <<-EOT
+    {% set results = [] -%}
+
     {%- if $.virtualMachine.managedDiskId == "" or $.virtualMachine.managedDiskId == null -%}
-    result: Not approved
-    message: "The VM is not using managed disks"
+
+      {%- set data = {
+          "title": extension.name,
+          "result": "Not approved",
+          "message": extension.name + " is not using managed disks"
+      } -%}
+
+    {%- elif $.virtualMachine.extensions -%}
+
+      {%- for extension in $.virtualMachine.extensions -%}
+
+        {%- if extension.name in $.installedExtensions -%}
+
+          {% set data = {
+              "title": extension.name,
+              "result": "Approved",
+              "message": extension.name + " is installed"
+          } -%}
+
+        {%- else -%}
+
+          {% set data = {
+              "title": extension.name,
+              "result": "Not approved",
+              "message": extension.name + " is not installed"
+          } -%}
+
+        {%- endif -%}
+
+        {% set results = results.concat(data) -%}
+
+      {% endfor -%}
+
     {%- else -%}
-    result: Approved
-    message: "The VM is using managed disks"
-    {%- endif -%}
-    EOT
+
+      {%- if $.virtualMachine.managedDiskId != "" and $.virtualMachine.managedDiskId != null -%}
+
+        {%- set results = {
+            "title": extension.name,
+            "result": "Approved",
+            "message": extension.name + " is using managed disks"
+        } -%}
+
+      {%- else -%}
+
+        {%- set results = {
+            "title": "Extensions",
+            "result": "Skip",
+            "message": "No data for VM yet"
+        } -%}
+
+      {%- endif -%}
+
+    {% endif -%}
+    {{ results | json }}
+  EOT
 }
