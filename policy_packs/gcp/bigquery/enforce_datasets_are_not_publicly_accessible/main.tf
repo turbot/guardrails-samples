00fc9988e3922bc3c4a8dec85a1af10d28ca568d
Sumit Popat <sumit@turbot.com>
2024-07-01 22:01:51 +0530

    update main for bq dataset.

--- policy_packs/gcp/bigquery/enforce_datasets_are_not_publicly_accessible/main.tf
+++ policy_packs/gcp/bigquery/enforce_datasets_are_not_publicly_accessible/main.tf
@@ -1,5 +1,5 @@
 resource "turbot_smart_folder" "main" {
-  title       = "Enforce GCP BigQuery Datasets Are Not Publicly Accessible"
+  title       = "Enforce GCP BigQuery Datasets To Not Be Publicly Accessible"
   description = "Protect sensitive and proprietary data from unauthorized access and potential breaches."
   parent      = "tmod:@turbot/turbot#/"
 }
