13eebd1ab75a387e7eaeae8096157d59140de177
Sumit Popat <sumit@turbot.com>
2024-07-01 21:56:25 +0530

    remove policy pack for gke clusters master auth network.

--- policy_packs/gcp/kubernetesengine/enforce_clusters_are_not_internet_exposed/main.tf
+++
@@ -1,5 +0,0 @@
-resource "turbot_smart_folder" "main" {
-  title       = "Enforce GCP GKE Clusters are Not Exposed to the Internet"
-  description = "Ensure Enforce GCP GKE clusters are not exposed to the internet for safeguarding against unauthorized access and potential security breaches."
-  parent      = "tmod:@turbot/turbot#/"
-}
