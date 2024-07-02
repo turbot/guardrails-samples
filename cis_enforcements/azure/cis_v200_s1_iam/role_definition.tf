1037ec6842937a55704855f0248d324a9c91b19c
madhushreeray30 <madhushree@turbot.com>
2024-07-02 17:39:58 +0530

    Policy Pack for Azure CIS v2.0 Enforcement for Section 1 - Identity and Access Management.

---
+++ cis_enforcements/azure/cis_v200_s1_iam/role_definition.tf
@@ -0,0 +1,38 @@
+# Azure > IAM > RoleDefinition > Approved
+resource "turbot_policy_setting" "azure_iam_role_definition_approved" {
+  resource = turbot_smart_folder.azure_cis_v200_s1_iam.id
+  type     = "tmod:@turbot/azure-iam#/policy/types/roleDefinitionApproved"
+  note     = "Azure CIS v2.0.0 - Control: 1.23"
+  value    = "Check: Approved"
+  # value    = "Enforce: Delete unapproved"
+}
+
+# Azure > IAM > RoleDefinition > Approved > Custom
+resource "turbot_policy_setting" "azure_iam_role_definition_approved_custom" {
+  resource       = turbot_smart_folder.azure_cis_v200_s1_iam.id
+  type           = "tmod:@turbot/azure-iam#/policy/types/roleDefinitionApprovedCustom"
+  note           = "Azure CIS v2.0.0 - Control: 1.23"
+  template       = <<-EOT
+    {%- set actionsCheck = false -%}
+    {%- for permission in $.roleDefinition.permissions -%}
+      {%- if "*" in permission.actions -%}
+        {%- set actionsCheck = true -%}
+      {%- endif -%}
+    {%- endfor -%}
+
+    {%- set assignableScopesCheck = false -%}
+    {%- for scope in $.roleDefinition.assignableScopes -%}
+      {%- if scope.match('^/subscriptions/[-a-f0-9]{36}$') or scope == '/' -%}
+        {%- set assignableScopesCheck = true -%}
+      {%- endif -%}
+    {%- endfor -%}
+
+    {%- if $.roleDefinition.roleType == "CustomRole" and assignableScopesCheck and actionsCheck -%}
+    result: Not approved
+    message: "Custom Subscription Administrator Roles exist"
+    {%- else -%}
+    result: Approved
+    message: "Custom Subscription Administrator Roles does not exist"
+    {%- endif -%}
+    EOT
+}
