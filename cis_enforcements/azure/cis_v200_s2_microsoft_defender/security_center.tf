e234ba62ff960ae9778d2f11f43fa1a1cbb97824
niharika2k00 <dniharika16@gmail.com>
2024-07-02 19:42:48 +0530

    update value

--- cis_enforcements/azure/cis_v200_s2_microsoft_defender/security_center.tf
+++ cis_enforcements/azure/cis_v200_s2_microsoft_defender/security_center.tf
@@ -3,9 +3,7 @@ resource "turbot_policy_setting" "azure_securitycenter_defender_plan" {
   resource = turbot_smart_folder.pack.id
   type     = "tmod:@turbot/azure-securitycenter#/policy/types/securityCenterDefenderPlan"
   note     = "Azure CIS v2.0.0 - Controls: 2.1.1, 2.1.2, 2.1.3, 2.1.4, 2.1.5, 2.1.6, 2.1.7, 2.1.8, 2.1.9, 2.1.10, 2.1.11, 2.1.12"
-  value    = "Check: Defender Plan Disabled"
-  # value    = "Check: Defender Plan Enabled"
-  # value    = "Enforce: Defender Plan Disabled"
+  value    = "Check: Defender Plan Enabled"
   # value    = "Enforce: Defender Plan Enabled"
 }
 
@@ -14,7 +12,25 @@ resource "turbot_policy_setting" "azure_securitycenter_defender_plan_resource_ty
   resource = turbot_smart_folder.pack.id
   type     = "tmod:@turbot/azure-securitycenter#/policy/types/securityCenterDefenderPlanResourceType"
   note     = "Azure CIS v2.0.0 - Controls: 2.1.1, 2.1.2, 2.1.3, 2.1.4, 2.1.5, 2.1.6, 2.1.7, 2.1.8, 2.1.9, 2.1.10, 2.1.11, 2.1.12"
-  value    = jsonencode(["Servers", "API", "App Service", "Azure SQL Databases", "Cloud Posture", "Container registries", "Containers", "Cosmos DB", "DNS", "Key Vault", "Kubernetes", "Open-source relational databases", "Resource Manager", "SQL servers on machines", "Storage"])
+  value    = <<EOT
+    [
+      "Servers",
+      "API",
+      "App Service",
+      "Azure SQL Databases",
+      "Cloud Posture",
+      "Container registries",
+      "Containers",
+      "Cosmos DB",
+      "DNS",
+      "Key Vault",
+      "Kubernetes",
+      "Open-source relational databases",
+      "Resource Manager",
+      "SQL servers on machines",
+      "Storage"
+    ]
+  EOT
 }
 
 # Azure > Security Center > Security Center > Auto Provisioning
@@ -22,8 +38,6 @@ resource "turbot_policy_setting" "azure_securitycenter_auto_provisioning" {
   resource = turbot_smart_folder.pack.id
   type     = "tmod:@turbot/azure-securitycenter#/policy/types/securityCenterAutoProvisioning"
   note     = "Azure CIS v2.0.0 - Control: 2.1.15"
-  value    = "Check: Disabled"
-  #   value    = "Check: Enabled"
-  #   value    = "Enforce: Disabled"
-  #   value    = "Enforce: Enabled"
+  value    = "Check: Enabled"
+  # value    = "Enforce: Enabled"
 }
