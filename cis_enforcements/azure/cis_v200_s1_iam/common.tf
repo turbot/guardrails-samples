04150e5a601da95a54fa7e6043d4098fe08b56b0
madhushreeray30 <madhushree@turbot.com>
2024-07-02 20:04:34 +0530

    add API enabled

--- cis_enforcements/azure/cis_v200_s1_iam/common.tf
+++ cis_enforcements/azure/cis_v200_s1_iam/common.tf
@@ -4,3 +4,10 @@ resource "turbot_smart_folder" "azure_cis_v200_s1_iam" {
   title       = "Azure CIS v2.0.0 - Section 1 - Identity and Access Management"
   description = "This section contains recommendations for configuring Azure Identity and Access Management features."
 }
+
+# Azure > IAM > Enabled
+resource "turbot_policy_setting" "azure_iam_enabled" {
+  resource = turbot_smart_folder.azure_cis_v200_s1_iam.id
+  type     = "tmod:@turbot/aws-iam#/policy/types/iamEnabled"
+  value    = "Enabled"
+}
