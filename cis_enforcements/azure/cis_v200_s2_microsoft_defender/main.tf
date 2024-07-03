4e249826f25d635c6b36fcc1cccd6662eae647a3
niharika2k00 <dniharika16@gmail.com>
2024-07-03 19:30:14 +0530

    removed enabled policy

--- cis_enforcements/azure/cis_v200_s2_microsoft_defender/main.tf
+++ cis_enforcements/azure/cis_v200_s2_microsoft_defender/main.tf
@@ -4,10 +4,3 @@ resource "turbot_smart_folder" "pack" {
   title       = "Azure CIS v2.0.0 - Section 2 - Microsoft Defender"
   description = "This section contains recommendations for configuring Azure Microsoft Defender features."
 }
-
-# Azure > Security Center > Enabled
-resource "turbot_policy_setting" "azure_securitycenter_enabled" {
-  resource = turbot_smart_folder.pack.id
-  type     = "tmod:@turbot/azure-securitycenter#/policy/types/securityCenterServiceEnabled"
-  value    = "Enabled"
-}
