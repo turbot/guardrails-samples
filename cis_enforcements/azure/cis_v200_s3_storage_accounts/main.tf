1b370e6ecf85cb36a7791a5e2bc5f75bf04783a6
niharika2k00 <dniharika16@gmail.com>
2024-07-03 19:17:41 +0530

    Revert "Policy Pack for Azure CIS v2.0 Enforcement for Section 3 - Storage Accounts."
    This reverts commit b838fb6b466a9d40e95e715f6e0b385cdc400e05.

--- cis_enforcements/azure/cis_v200_s3_storage_accounts/main.tf
+++
@@ -1,6 +0,0 @@
-# Policy Pack
-resource "turbot_smart_folder" "pack" {
-  title       = "Azure CIS v2.0.0 - Section 3 - Storage Accounts"
-  description = "This section contains recommendations for configuring Azure Storage Accounts features."
-  parent      = "tmod:@turbot/turbot#/"
-}
