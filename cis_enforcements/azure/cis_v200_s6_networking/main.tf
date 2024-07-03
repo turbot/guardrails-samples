3b35e7dbe083899ed5421c7101679cb9e19f81e5
niharika2k00 <dniharika16@gmail.com>
2024-07-03 19:34:24 +0530

    Revert "Policy Pack for Azure CIS v2.0 Enforcement for Section 6 - Networking."
    This reverts commit 0d4c6aa1bf58185baaec0e583483e64bb004cd94.

--- cis_enforcements/azure/cis_v200_s6_networking/main.tf
+++
@@ -1,6 +0,0 @@
-# Policy Pack
-resource "turbot_smart_folder" "pack" {
-  parent      = "tmod:@turbot/turbot#/"
-  title       = "Azure CIS v2.0.0 - Section 6 - Networking"
-  description = "This section contains recommendations for configuring Azure Networking features."
-}
