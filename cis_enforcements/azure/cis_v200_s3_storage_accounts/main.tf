55d31a73e42f3c8a9e1129c8162e9a79578f92cd
niharika2k00 <dniharika16@gmail.com>
2024-07-04 18:54:35 +0530

    update

--- cis_enforcements/azure/cis_v200_s3_storage_accounts/main.tf
+++ cis_enforcements/azure/cis_v200_s3_storage_accounts/main.tf
@@ -1,6 +1,6 @@
-# Policy Pack
+# Smart Folder
 resource "turbot_smart_folder" "pack" {
   title       = "Azure CIS v2.0.0 - Section 3 - Storage Accounts"
-  description = "This section contains recommendations for configuring Azure Storage Accounts features."
+  description = "This section covers security recommendations to follow to set storage account policies on an Azure Subscription. An Azure storage account provides a unique namespace to store and access Azure Storage data objects."
   parent      = "tmod:@turbot/turbot#/"
 }
