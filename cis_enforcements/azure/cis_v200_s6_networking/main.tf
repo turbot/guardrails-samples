0f865778bfd4a04fc431f699fa8c82001f244465
niharika2k00 <dniharika16@gmail.com>
2024-07-04 18:55:18 +0530

    update

--- cis_enforcements/azure/cis_v200_s6_networking/main.tf
+++ cis_enforcements/azure/cis_v200_s6_networking/main.tf
@@ -1,6 +1,6 @@
-# Policy Pack
+# Smart Folder
 resource "turbot_smart_folder" "pack" {
   parent      = "tmod:@turbot/turbot#/"
   title       = "Azure CIS v2.0.0 - Section 6 - Networking"
-  description = "This section contains recommendations for configuring Azure Networking features."
+  description = "This section covers security recommendations to follow in order to set networking policies on an Azure subscription."
 }
