fa3223281a0249d93b68bed07bb652412eebf7dc
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-07-05 17:56:07 +0530

    Policy Pack for Azure CIS v2.0 Enforcement for Section 8 - Key Vault (#748)

---
+++ cis_enforcements/azure/cis_v200/section8/main.tf
@@ -0,0 +1,6 @@
+# Smart Folder
+resource "turbot_smart_folder" "main" {
+  parent      = "tmod:@turbot/turbot#/"
+  title       = "Azure CIS v2.0.0 - Section 8 - Key Vault"
+  description = "This section covers security recommendations to follow for the configuration and use of Azure Key Vault."
+}
