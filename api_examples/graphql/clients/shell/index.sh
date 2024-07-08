4d3f9e4283b3778d8a0afa06b87596d7f2517958
Cody Bruno <cody@turbot.com>
2024-07-08 14:03:36 -0400

    Remove directories moved to guardrails-tools

--- api_examples/graphql/clients/shell/index.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash -x
-
-curl \
-  --request POST \
-  "${TURBOT_GRAPHQL_ENDPOINT}" \
-  --user "${TURBOT_ACCESS_KEY_ID}:${TURBOT_SECRET_ACCESS_KEY}" \
-  --header "Content-Type: application/json" \
-  --verbose \
-  --data @query.json
--- api_examples/graphql/queries/mods/notes.md
+++
@@ -1,5 +0,0 @@
-# Notes
-
-This whole section could be cleaned up.
-The queries could be written into a tutorial format so that the usage of the queries can be demonstrated.
-Each query could live in its own file.
