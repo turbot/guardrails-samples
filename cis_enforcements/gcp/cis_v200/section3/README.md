ad3d9591c1e1d14e93ada9b64e6d580c18516ffb
sourav chakraborty <sourav@turbot.com>
2024-07-08 20:48:23 +0530

    Add doc and update folder structure

---
+++ cis_enforcements/gcp/cis_v200/section3/README.md
@@ -0,0 +1,118 @@
+---
+categories: ["cis"]
+---
+
+# Enforce GCP CIS v2.0.0 - Section 3 - Networking
+
+This section covers recommendations addressing networking on Google Cloud Platform.
+
+This policy pack can help you automate enforcement of GCP CIS benchmark section 3 best practices.
+
+**[Review policy settings →](https://hub-guardrails-turbot-com-git-development-turbot.vercel.app/policy-packs/gcp/cis_v200/section3/settings)**
+
+## Getting Started
+
+### Requirements
+
+- [Terraform](https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/install-cli)
+- Guardrails mods:
+  - [@turbot/gcp-network](https://hub-guardrails-turbot-com-git-development-turbot.vercel.app/gcp/mods/gcp-network)
+  - [@turbot/gcp-dns](https://hub-guardrails-turbot-com-git-development-turbot.vercel.app/gcp/mods/gcp-dns)
+
+### Credentials
+
+To create a policy pack through Terraform:
+
+- Ensure you have `Turbot/Admin` permissions (or higher) in Guardrails
+- [Create access keys](https://turbot.com/guardrails/docs/guides/iam/access-keys#generate-a-new-guardrails-api-access-key) in Guardrails
+
+And then set your credentials:
+
+```sh
+export TURBOT_WORKSPACE=myworkspace.acme.com
+export TURBOT_ACCESS_KEY=acce6ac5-access-key-here
+export TURBOT_SECRET_KEY=a8af61ec-secret-key-here
+```
+
+Please see [Turbot Guardrails Provider authentication](https://registry.terraform.io/providers/turbot/turbot/latest/docs#authentication) for additional authentication methods.
+
+## Usage
+
+### Install Policy Pack
+
+> [!NOTE]
+> By default, installed policy packs are not attached to any resources.
+>
+> Policy packs must be attached to resources in order for their policy settings to take effect.
+> Clone:
+
+```sh
+git clone https://github.com/turbot/guardrails-samples.git
+cd guardrails-samples/policy_packs/gcp/cis_v200/section3
+```
+
+Run the Terraform to create the policy pack in your workspace:
+
+```sh
+terraform init
+terraform plan
+```
+
+Then apply the changes:
+
+```sh
+terraform apply
+```
+
+### Apply Policy Pack
+
+Log into your Guardrails workspace and [attach the policy pack to a resource](https://turbot.com/guardrails/docs/guides/working-with-folders/smart#attach-a-smart-folder-to-a-resource).
+
+### Enable Enforcement
+
+> [!TIP]
+> You can also update the policy settings in this policy pack directly in the Guardrails console.
+>
+> Please note your Terraform state file will then become out of sync and the policy settings should then only be managed in the console.
+> By default, the policies are set to `Check` in the pack's policy settings. To enable automated enforcements, you can switch these policies settings by adding a comment to the `Check` setting and removing the comment from one of the listed enforcement options:
+
+```hcl
+resource "turbot_policy_setting" "gcp_network_approved" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/networkApproved"
+  note     = "GCP CIS v2.0.0 - Control: 3.1"
+  # value    = "Check: Approved"
+  value    = "Enforce: Delete unapproved if new"
+}
+
+resource "turbot_policy_setting" "gcp_network_firewall_ingress_rules_approved" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/firewallIngressRulesApproved"
+  note     = "GCP CIS v2.0.0 - Control: 3.6 and 3.7"
+  # value    = "Check: Approved"
+  value    = "Enforce: Delete unapproved"
+}
+
+resource "turbot_policy_setting" "gcp_network_firewall_approved" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/firewallApproved"
+  note     = "GCP CIS v2.0.0 - Control: 3.10"
+  # value    = "Check: Approved"
+  value    = "Enforce: Delete unapproved if new"
+}
+
+resource "turbot_policy_setting" "gcp_dns_managed_zone_approved" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-dns#/policy/types/managedZoneApproved"
+  note     = "GCP CIS v2.0.0 - Control: 3.4 and 3.5"
+  # value    = "Check: Approved"
+  value    = "Enforce: Delete unapproved if new"
+}
+```
+
+Then re-apply the changes:
+
+```sh
+terraform plan
+terraform apply
+```
---
+++ cis_enforcements/gcp/cis_v200/section3/network.tf
@@ -0,0 +1,256 @@
+# GCP > Network > Approved
+resource "turbot_policy_setting" "gcp_network_approved" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/networkApproved"
+  note     = "GCP CIS v2.0.0 - Control: 3.1"
+  value    = "Check: Approved"
+  # value    = "Enforce: Delete unapproved if new"
+}
+
+# GCP > Network > Approved > Custom
+resource "turbot_policy_setting" "gcp_network_approved_custom" {
+  resource       = turbot_smart_folder.main.id
+  type           = "tmod:@turbot/gcp-network#/policy/types/networkApprovedCustom"
+  note           = "Azure CIS v2.0.0 - Control: 3.1 and 3.2"
+  template_input = <<-EOT
+    {
+      network {
+        name
+        autoCreateSubnetworks: get(path: "autoCreateSubnetworks")
+      }
+    }
+  EOT
+  template       = <<-EOT
+    {%- set results = [] -%}
+
+    {%- if $.network.name == "default" -%}
+
+      {%- set data = {
+          "title": "Network",
+          "result": "Not approved",
+          "message": "Default network"
+      } -%}
+
+    {%- elif $.network.name != "default" -%}
+
+      {%- set data = {
+          "title": "Network",
+          "result": "Approved",
+          "message": "Not a default network"
+      } -%}
+
+    {%- else -%}
+
+      {%- set data = {
+          "title": "Network",
+          "result": "Skip",
+          "message": "No data for network yet"
+      } -%}
+
+    {%- endif -%}
+
+    {%- set results = results.concat(data) -%}
+
+    {%- if $.network.autoCreateSubnetworks == null -%}
+
+      {%- set data = {
+          "title": "Network",
+          "result": "Not approved",
+          "message": "Legacy network"
+      } -%}
+
+    {%- elif $.network.autoCreateSubnetworks != null -%}
+
+      {%- set data = {
+          "title": "Network",
+          "result": "Approved",
+          "message": "Not a legacy network"
+      } -%}
+
+    {%- else -%}
+
+      {%- set data = {
+          "title": "Subnetworks",
+          "result": "Skip",
+          "message": "No data for AutoCreateSubnetworks yet"
+      } -%}
+
+    {% endif -%}
+
+    {% set results = results.concat(data) -%}
+
+    {{ results | json }}
+  EOT
+}
+
+# GCP > Network > Firewall > Ingress Rules > Approved
+resource "turbot_policy_setting" "gcp_network_firewall_ingress_rules_approved" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/firewallIngressRulesApproved"
+  note     = "GCP CIS v2.0.0 - Control: 3.6 and 3.7"
+  value    = "Check: Approved"
+  # value    = "Enforce: Delete unapproved"
+}
+
+# GCP > Network > Firewall > Ingress Rules > Approved Rules
+resource "turbot_policy_setting" "gcp_network_firewall_ingress_rules_approved_rules" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/firewallIngressRulesApprovedRules"
+  note     = "GCP CIS v2.0.0 - Control: 3.6 and 3.7"
+  value    = <<EOT
+    REJECT $.turbot.cidr:0.0.0.0/0 $.turbot.ports=22, 3389
+
+    APPROVE *
+  EOT
+}
+
+# GCP > Network > SSL Policy > Minimum TLS Version
+resource "turbot_policy_setting" "gcp_network_ssl_policy_minimum_tls_version" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/sslPolicyMinimumTlsVersion"
+  note     = "GCP CIS v2.0.0 - Control: 3.9"
+  value    = "Enforce: TLS 1.2"
+}
+
+# GCP > Network > SSL Policy > Profile
+resource "turbot_policy_setting" "gcp_network_ssl_policy_profile" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/sslPolicyProfile"
+  note     = "GCP CIS v2.0.0 - Control: 3.9"
+  value    = "Enforce: Restricted"
+}
+
+# GCP > Network > Firewall > Approved
+resource "turbot_policy_setting" "gcp_network_firewall_approved" {
+  resource = turbot_smart_folder.main.id
+  type     = "tmod:@turbot/gcp-network#/policy/types/firewallApproved"
+  note     = "GCP CIS v2.0.0 - Control: 3.10"
+  value    = "Check: Approved"
+  # value    = "Enforce: Delete unapproved if new"
+}
+
+# GCP > Network > Firewall > Approved > Custom
+resource "turbot_policy_setting" "gcp_network_firewall_approved_custom" {
+  resource       = turbot_smart_folder.main.id
+  type           = "tmod:@turbot/gcp-network#/policy/types/firewallApprovedCustom"
+  note           = "Azure CIS v2.0.0 - Control: 3.10"
+  template_input = <<-EOT
+    {
+      firewall {
+        sourceRanges: get(path: "sourceRanges")
+        allowed: get(path: "allowed")
+      }
+    }
+  EOT
+  template       = <<-EOT
+    {%- set results = [] -%}
+
+    {%- if $.firewall.sourceRanges == null -%}
+
+      {%- set data = {
+          "title": "Source Ranges",
+          "result": "Not approved",
+          "message": "No source ranges defined"
+      } -%}
+
+    {%- elif $.firewall.sourceRanges != null -%}
+
+      {%- set allowedSourceIPRanges = ["35.235.240.0/20", "130.211.0.0/22", "35.191.0.0/16"] -%}
+      {%- set allRequiredIpExist = true -%}
+
+      {%- for ip in allowedSourceIPRanges -%}
+        {%- if ip not in $.firewall.sourceRanges -%}
+          {%- set allRequiredIpExist = false -%}
+        {%- endif -%}
+      {%- endfor -%}
+
+      {%- if allRequiredIpExist -%}
+
+        {%- set data = {
+            "title": "Source Ranges",
+            "result": "Approved",
+            "message": "All required source ranges are defined"
+        } -%}
+
+      {%- else -%}
+
+        {%- set data = {
+            "title": "Source Ranges",
+            "result": "Not approved",
+            "message": "Missing required source ranges"
+        } -%}
+
+      {%- endif -%}
+
+      {% set results = results.concat(data) -%}
+
+    {%- else -%}
+
+      {%- set data = {
+          "title": "Source Ranges",
+          "result": "Skip",
+          "message": "No data for source ranges yet"
+      } -%}
+
+      {% set results = results.concat(data) -%}
+
+    {%- endif -%}
+
+    {%- if $.firewall.allowed == null -%}
+
+      {%- set data = {
+          "title": "Allowed Protocols",
+          "result": "Not approved",
+          "message": "No allowed protocols defined"
+      } -%}
+
+    {%- elif $.firewall.allowed != null -%}
+
+      {%- set desiredPorts = ["22", "80", "443", "3389"] -%}
+      {%- set allowProtocolsAndPorts = false -%}
+
+      {%- for allow in $.firewall.allowed -%}
+        {%- if allow.IPProtocol == "tcp" and allow.ports is defined -%}
+          {%- for port in allow.ports -%}
+            {%- if port in desiredPorts -%}
+              {%- set allowProtocolsAndPorts = true -%}
+            {%- endif -%}
+          {%- endfor -%}
+        {%- endif -%}
+      {%- endfor -%}
+
+      {%- if allowProtocolsAndPorts -%}
+
+        {%- set data = {
+            "title": "Allowed Ports",
+            "result": "Approved",
+            "message": "Required ports are allowed"
+        } -%}
+
+      {%- else -%}
+
+        {%- set data = {
+            "title": "Allowed Ports",
+            "result": "Not approved",
+            "message": "Required ports are not allowed"
+        } -%}
+
+      {%- endif -%}
+
+      {% set results = results.concat(data) -%}
+
+    {%- else -%}
+
+      {%- set data = {
+          "title": "Allowed Protocols",
+          "result": "Skip",
+          "message": "No data for allowed protocols yet"
+      } -%}
+
+      {% set results = results.concat(data) -%}
+
+    {%- endif -%}
+
+    {{ results | json }}
+  EOT
+}
