9bda2270d354a0126a7f93bb1e0b24dd14274d0f
Cody Bruno <cody@turbot.com>
2024-07-08 14:29:38 -0400

    Remove unused issue templates, vscode settings, and trimbot directory

--- trimbot/trimbot_modules/__init__.py
+++
@@ -1,6 +0,0 @@
-from .configuration import Configuration
-from .session import Session
-from .v3_api import V3Api
-from .resource_service_factory import ResourceServiceFactory
-from .actions import CheckAction, NoCheckAction
-from .recipe import Recipe
