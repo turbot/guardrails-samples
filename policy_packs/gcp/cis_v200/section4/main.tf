48d5909b328625c4f2ebd0fe504800cc64ed1612
Sumit Popat <sumit@turbot.com>
2024-07-10 23:05:03 +0530

    remove comment on main.tf

--- policy_packs/gcp/cis_v200/section4/main.tf
+++ policy_packs/gcp/cis_v200/section4/main.tf
@@ -1,4 +1,3 @@
-# Smart Folder
 resource "turbot_smart_folder" "main" {
   title       = "GCP CIS v2.0.0 - Section 4 - Virtual Machines"
   description = "This section covers recommendations addressing virtual machines on Google Cloud Platform."
