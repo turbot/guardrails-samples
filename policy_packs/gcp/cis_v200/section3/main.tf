c86937845204ddc61d75bdbeff6c27c89b0759de
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-07-10 22:39:25 +0530

    Policy Pack for GCP CIS v2.0 Enforcement for Section 3 - Networking (#757)

---
+++ policy_packs/gcp/cis_v200/section3/main.tf
@@ -0,0 +1,6 @@
+# Smart Folder
+resource "turbot_smart_folder" "main" {
+  title       = "GCP CIS v2.0.0 - Section 3 - Networking"
+  description = "This section covers recommendations addressing networking on Google Cloud Platform."
+  parent      = "tmod:@turbot/turbot#/"
+}
