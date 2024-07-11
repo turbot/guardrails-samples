e2226362d81c2eba5e393e4c8ed62a3744492292
Sumit Popat <sumit@turbot.com>
2024-07-11 17:00:59 +0530

    Update main.tf

--- policy_packs/gcp/cis_v200/section7/main.tf
+++ policy_packs/gcp/cis_v200/section7/main.tf
@@ -1,4 +1,3 @@
-# Smart Folder
 resource "turbot_smart_folder" "main" {
   title       = "GCP CIS v2.0.0 - Section 7 - BigQuery"
   description = "This section addresses Google CloudPlatform BigQuery. BigQuery is a serverless, highly-scalable, and cost-effective cloud data warehouse with an in-memory BI Engine and machine learning built in."
