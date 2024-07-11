ff977d4cc01c041a1f3c21b4622c515e3b039e92
Sumit Popat <sumit@turbot.com>
2024-07-11 22:41:24 +0530

    Update policies.tf

--- policy_packs/gcp/cis_v200/section7/policies.tf
+++ policy_packs/gcp/cis_v200/section7/policies.tf
@@ -41,8 +41,8 @@ resource "turbot_policy_setting" "gcp_bigquery_dataset_encryption_at_rest" {
   type     = "tmod:@turbot/gcp-bigquery#/policy/types/datasetEncryptionAtRest"
   note     = "GCP CIS v2.0.0 - Control: 7.3"
   value    = "Check: Customer managed key"
-  # value    = "Enforce: Customer managed key"
   # value    = "Check: Encryption at Rest > Customer Managed Key"
+  # value    = "Enforce: Customer managed key"
   # value    = "Enforce: Encryption at Rest > Customer Managed Key"
 }
 
@@ -51,6 +51,6 @@ resource "turbot_policy_setting" "gcp_bigquery_dataset_encryption_at_rest_custom
   resource = turbot_smart_folder.main.id
   type     = "tmod:@turbot/gcp-bigquery#/policy/types/datasetEncryptionAtRestCustomerManagedKey"
   note     = "GCP CIS v2.0.0 - Control: 7.3"
-  # The ID of a GCP KMS symmetric key
-  value = "projects/parker-aac/locations/us/keyRings/test-cis-keyring/cryptoKeys/test-cis-key"
+  # The ID of your GCP KMS key
+  value = "projects/my-project/locations/us-east1/keyRings/my-keyring/cryptoKeys/my-key"
 }
