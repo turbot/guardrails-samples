32204df88bd78ee8544b6133ea26cb058522a128
Sumit Popat <sumit@turbot.com>
2024-07-15 23:10:28 +0530

    dix aka for gcp policy pack.

--- policy_packs/azure/cis_v200/section3/main.tf
+++ policy_packs/azure/cis_v200/section3/main.tf
@@ -1,4 +1,3 @@
-# Smart Folder
 resource "turbot_policy_pack" "main" {
   title       = "Azure CIS v2.0.0 - Section 3 - Storage Accounts"
   description = "This section covers security recommendations to follow to set storage account policies on an Azure Subscription. An Azure storage account provides a unique namespace to store and access Azure Storage data objects."
--- policy_packs/gcp/cis_v200/section2/main.tf
+++ policy_packs/gcp/cis_v200/section2/main.tf
@@ -1,4 +1,3 @@
-# Smart Folder
 resource "turbot_policy_pack" "main" {
   title       = "GCP CIS v2.0.0 - Section 2 - Logging and Monitoring"
   description = "This section covers recommendations addressing Logging and Monitoring on Google Cloud Platform."
