57a0d5810e0f436ef2b2d9c8feef3258e0676583
niharika2k00 <dniharika16@gmail.com>
2024-07-16 20:22:20 +0530

    update mod name

--- policy_packs/azure/compute/enforce_vms_to_not_be_older_than_7_days/policies.tf
+++ policy_packs/azure/compute/enforce_vms_to_not_be_older_than_7_days/policies.tf
@@ -1,7 +1,7 @@
 # Azure > Compute > Virtual Machine > Active
 resource "turbot_policy_setting" "azure_compute_virtual_machine_active" {
   resource = turbot_policy_pack.main.id
-  type     = "tmod:@turbot/azure-storage#/policy/types/virtualMachineActive"
+  type     = "tmod:@turbot/azure-compute#/policy/types/virtualMachineActive"
   value    = "Check: Active"
   # value    = "Enforce: Delete inactive with 7 days warning"
 }
@@ -9,6 +9,6 @@ resource "turbot_policy_setting" "azure_compute_virtual_machine_active" {
 # Azure > Compute > Virtual Machine > Active > Age
 resource "turbot_policy_setting" "azure_compute_virtual_machine_active_age" {
   resource = turbot_policy_pack.main.id
-  type     = "tmod:@turbot/azure-storage#/policy/types/virtualMachineActiveAge"
+  type     = "tmod:@turbot/azure-compute#/policy/types/virtualMachineActiveAge"
   value    = "Force inactive if age > 7 days"
 }
