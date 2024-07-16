dc724fa6a1c3ca41f95638c39edbf635df16057e
sourav chakraborty <sourav@turbot.com>
2024-07-16 13:54:02 +0530

    Add policy packs for AWS VPC

---
+++ policy_packs/aws/vpc/enforce_block_public_access_for_security_groups/policies.tf
@@ -0,0 +1,21 @@
+# AWS > VPC > Security Group > Ingress Rules > Approved
+resource "turbot_policy_setting" "aws_vpc_security_security_group_ingress_rules_approved" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-vpc-security#/policy/types/securityGroupIngressRulesApproved"
+  value    = "Check: Approved"
+  # value   = "Enforce: Delete unapproved"
+}
+
+# AWS > VPC > Security Group > Ingress Rules > Approved > Rules
+resource "turbot_policy_setting" "aws_vpc_security_security_group_ingress_rules_approved_rules" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-vpc-security#/policy/types/securityGroupIngressRulesApprovedRules"
+  value    = <<-EOT
+    # Reject port 22 from internet
+    REJECT $.turbot.fromPort:=22 $.turbot.toPort:=22 $.turbot.cidr:0.0.0.0/0,::/0
+    REJECT $.turbot.portRangeSize:-1  $.turbot.cidr:0.0.0.0/0,::/0
+
+    # APPROVE unmatched rules
+    APPROVE *
+    EOT
+}
