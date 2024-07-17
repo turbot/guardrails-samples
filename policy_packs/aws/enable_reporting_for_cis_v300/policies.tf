00c152d663b626aaab2430f1b3eb377687afa05d
Sumit Popat <sumit@turbot.com>
2024-07-17 19:50:29 +0530

    add policy pack to enable aws cis v300 in guardrails.

---
+++ policy_packs/aws/enable_reporting_for_cis_v300/policies.tf
@@ -0,0 +1,16 @@
+# AWS > CIS v3.0
+resource "turbot_policy_setting" "aws_cis_v300" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-cisv3-0#/policy/types/cis"
+  # value    = "Check: All CIS Benchmarks"
+  value    = "Check: All CIS Benchmarks except attestations"
+}
+
+# AWS > CIS v3.0 > Maximum Attestation Duration
+# resource "turbot_policy_setting" "aws_cis_v300_attestation" {
+#   resource = turbot_smart_folder.main.id
+#   type     = "tmod:@turbot/aws-cisv3-0#/policy/types/attestation"
+#   # value    = "90 days"
+#   value    = "1 year"
+#   # value    = "2 years"
+# }
