09f21f4ccafd2a8d726ecac95f562800ae771e4c
sourav chakraborty <sourav@turbot.com>
2024-07-17 13:31:28 +0530

    update policy pack for enforce_block_actions_from_unapproved_cidrs_for_security_groups

--- policy_packs/aws/vpc/enforce_block_actions_from_unapproved_cidrs_for_security_groups/policies.tf
+++ policy_packs/aws/vpc/enforce_block_actions_from_unapproved_cidrs_for_security_groups/policies.tf
@@ -7,10 +7,11 @@ resource "turbot_policy_setting" "aws_iam_iam_stack" {
 }
 
 # AWS > IAM > Role > Boundary > Policy
-resource "turbot_policy_setting" "aws_iam_role_boundary_policy_preventative" {
+resource "turbot_policy_setting" "aws_iam_role_boundary_policy" {
   resource = turbot_policy_pack.main.id
   type     = "tmod:@turbot/aws-iam#/policy/types/roleBoundaryPolicy"
-  value    = "testRole"
+  # Boundary policy name that will be applied to the IAM role.
+  value = "myBoundaryPolicy"
 }
 
 # AWS > IAM > Stack > Source
@@ -20,9 +21,10 @@ resource "turbot_policy_setting" "aws_iam_iam_stack_source" {
   value    = <<EOT
     ### Deny Actions from unapproved CIDRs ###
     resource "aws_iam_policy" "main" {
-      name        = "testRole"
+      # Boundary policy name that will be applied to the IAM role.
+      name        = "myBoundaryPolicy"
       path        = "/"
-      description = "Turbot Managed Boundary policy to prevent actions from  unapproved CIDRs"
+      description = "Guardrails Managed Boundary policy to prevent actions from  unapproved CIDRs"
       policy      = jsonencode({
         "Version": "2012-10-17",
         "Statement": [
@@ -50,7 +52,7 @@ resource "turbot_policy_setting" "aws_iam_iam_stack_source" {
 
 # AWS > IAM > Role > Boundary
 # Skip Boundary for Turbot IAM Role that is used to import the account.
-resource "turbot_policy_setting" "ec2_sgr_deny_boundary_preventative" {
+resource "turbot_policy_setting" "aws_iam_role_boundary" {
   resource       = turbot_policy_pack.main.id
   type           = "tmod:@turbot/aws-iam#/policy/types/roleBoundary"
   template_input = <<EOT
@@ -66,10 +68,14 @@ resource "turbot_policy_setting" "ec2_sgr_deny_boundary_preventative" {
   }
 EOT
   template       = <<EOT
-  {%- if $.resource.role_arn  == $.resource.parent.turbot_iam_role.value -%}
-    "Skip"
-  {%- else -%}
+  {%- if $.resource.role_arn and $.resource.role_arn  != $.resource.parent.turbot_iam_role.value -%}
+
     "Enforce: Boundary > Policy"
+
+  {%- else -%}
+
+    "Skip"
+
   {%- endif -%}
 EOT
 }
