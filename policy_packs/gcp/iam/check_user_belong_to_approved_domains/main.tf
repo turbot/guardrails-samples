89efe4f2652d034c8d3cf0d13853d09bfb219dfd
sourav chakraborty <sourav@turbot.com>
2024-07-18 11:04:53 +0530

    Add few more policy packs

---
+++ policy_packs/gcp/iam/check_user_belong_to_approved_domains/main.tf
@@ -0,0 +1,5 @@
+resource "turbot_policy_pack" "main" {
+  title       = "Check GCP IAM Users Belong To Approved Domains"
+  description = "Ensures that users belong to approved domains, reducing the risk of unauthorized access, data breaches, and malicious activities by ensuring that user accounts are managed within a secure and trusted environment."
+  akas        = ["gcp_iam_check_user_belong_to_approved_domains"]
+}
