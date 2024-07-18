56a9ad8294fd71089f42b715411d9780dfe728ff
Sumit Popat <sumit@turbot.com>
2024-07-18 15:21:31 +0530

    Update policies.tf

--- policy_packs/aws/logs/enforce_encryption_at_rest_is_enabled_for_log_groups/policies.tf
+++ policy_packs/aws/logs/enforce_encryption_at_rest_is_enabled_for_log_groups/policies.tf
@@ -8,7 +8,6 @@ resource "turbot_policy_setting" "aws_logs_log_group_encryption_at_rest" {
   # value    = "Enforce: AWS SSE or higher"
   # value    = "Enforce: Customer managed key"
   # value    = "Enforce: Encryption at Rest > Customer Managed Key"
-
 }
 
 # AWS > Logs > Log Group > Encryption at Rest > Customer Managed Key
@@ -21,8 +20,3 @@ resource "turbot_policy_setting" "aws_logs_log_group_encryption_at_rest_customer
   # value    = "arn:aws:kms:us-east-1:123456789012:key/ddc06e04-ce5f-4995-c758-c2b6c510e8fd"
   # value    = "arn:aws:kms:us-east-1:123456789012:alias/aws/ebs"
 }
-
-
-
-
-
