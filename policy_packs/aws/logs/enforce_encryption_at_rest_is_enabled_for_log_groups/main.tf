2f73047642de5066ff0fb74f8ad7ac8f1e1ff39d
Sumit Popat <sumit@turbot.com>
2024-07-18 15:20:45 +0530

    Update main.tf

--- policy_packs/aws/logs/enforce_encryption_at_rest_is_enabled_for_log_groups/main.tf
+++ policy_packs/aws/logs/enforce_encryption_at_rest_is_enabled_for_log_groups/main.tf
@@ -1,5 +1,5 @@
 resource "turbot_policy_pack" "main" {
-  title       = "Enforce Encryption at Rest Is Enabled for Log Groups"
+  title       = "Enforce Encryption at Rest Is Enabled for AWS Log Groups"
   description = "Ensure that CloudWatch log groups are encrypted for enhanced security."
   akas        = ["aws_logs_enforce_encryption_at_rest_is_enabled_for_log_groups"]
 }
