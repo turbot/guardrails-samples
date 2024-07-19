0ff80ae170c5ece112ca32d49ed2d691bf451be0
RahulLah19 <47312740+RahulLah19@users.noreply.github.com>
2024-07-19 23:36:54 +0530

    Add AWS VPC Policy Packs 2 (#791)

---
+++ policy_packs/aws/vpc/enforce_nat_gateways_to_not_exist/main.tf
@@ -0,0 +1,5 @@
+resource "turbot_policy_pack" "main" {
+  title       = "Enforce NAT Gateways To Not Exist"
+  description = "This measure prevents any outbound internet traffic from private subnets, reducing the risk of data exfiltration, unauthorized access, and potential security breaches."
+  akas        = ["aws_vpc_enforce_nat_gateways_to_not_exist"]
+}
