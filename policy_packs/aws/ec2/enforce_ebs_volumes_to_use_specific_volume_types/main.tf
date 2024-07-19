c5307c9f437da59f7e1b4a177c937c01ed6241d9
niharika2k00 <dniharika16@gmail.com>
2024-07-20 00:35:56 +0530

    AWS EC2 Policy Packs - Part 2.

---
+++ policy_packs/aws/ec2/enforce_ebs_volumes_to_use_specific_volume_types/main.tf
@@ -0,0 +1,5 @@
+resource "turbot_policy_pack" "main" {
+  title       = "Enforce AWS EC2 EBS Volumes To Use Specific Volume Types"
+  description = "Ensuring that only approved volume types are used, organizations can maintain consistent performance standards, manage expenses effectively, and mitigate risks associated with unauthorized or inappropriate storage configurations."
+  akas        = ["aws_ec2_enforce_ebs_volumes_to_use_specific_volume_types"]
+}
