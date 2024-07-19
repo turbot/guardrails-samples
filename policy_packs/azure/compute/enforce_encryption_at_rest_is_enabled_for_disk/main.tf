5b0bdb7bd6270b5adabe549676b811244bae1ee7
RahulLah19 <rahul.laheri@turbot.com>
2024-07-19 13:18:38 +0530

    Add Azure Compute Policy Pack

---
+++ policy_packs/azure/compute/enforce_encryption_at_rest_is_enabled_for_disk/main.tf
@@ -0,0 +1,5 @@
+resource "turbot_policy_pack" "main" {
+  title       = "Enforce Encryption at Rest is Enabled for Azure Compute Disk"
+  description = "Ensures that data remains confidential and secure, even if the physical storage media is compromised."
+  akas        = ["azure_compute_enforce_encryption_at_rest_is_enabled_for_disk"]
+}
