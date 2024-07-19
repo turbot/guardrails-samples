e7603b37928cf4874550f0c88c13adbc58c8905e
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2024-07-20 00:23:52 +0530

    Add AWS GuardDuty and Lambda Policy Packs. (#786)

---
+++ policy_packs/aws/guardduty/enforce_detectors_have_memberships_to_specific_accounts/main.tf
@@ -0,0 +1,5 @@
+resource "turbot_policy_pack" "main" {
+  title       = "Enforce AWS GuardDuty Detectors Have Membership Only To Specific Accounts"
+  description = "Ensure that GuardDuty findings and data are shared only with authorized accounts, enhancing security by preventing unauthorized access to sensitive security information."
+  akas        = ["aws_guardduty_enforce_detectors_have_memberships_to_specific_accounts"]
+}
