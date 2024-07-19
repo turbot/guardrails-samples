65ded6ff61f1413dc1a2a8dfdf229591efd2bf8c
madhushreeray30 <madhushree@turbot.com>
2024-07-19 17:28:57 +0530

    Add policy > aws_iam_restrict_policies

---
+++ policy_packs/aws/iam/enforce_policies_to_not_have_admin_privileges/policies.tf
@@ -0,0 +1,14 @@
+# AWS > IAM > Policy > Statements > Approved
+resource "turbot_policy_setting" "aws_iam_policy_statements_approved" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-iam#/policy/types/statementsApproved"
+  value    = "Check: Approved"
+  # value    = "Enforce: Delete Unapproved"
+}
+
+# AWS > IAM > Policy > Statements > Approved > Administrator Access
+resource "turbot_policy_setting" "aws_iam_policy_statements_approved_adminaccess" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-iam#/policy/types/statementsApprovedAdminAccess"
+  value    = "Disabled: Disallow Administrator Access ('*:*') policies"
+}
