a6918fba63d03983e4db1564a0586a95dc9acffa
SumitPopat <sumit@turbot.com>
2024-07-20 17:57:37 +0530

    Add policy pack to enable GCP CIS reporting.

---
+++ policy_packs/gcp/guardrails/enable_reporting_for_cis_v200/policies.tf
@@ -0,0 +1,16 @@
+# GCP > CIS v2.0
+resource "turbot_policy_setting" "gcp_cis_v200" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/gcp-cisv2-0#/policy/types/cis"
+  value    = "Check: All CIS Benchmarks except attestations"
+  # value    = "Check: All CIS Benchmarks"
+}
+
+# GCP > CIS v2.0 > Maximum Attestation Duration
+# resource "turbot_policy_setting" "gcp_cis_v200_attestation" {
+#   resource = turbot_smart_folder.main.id
+#   type     = "tmod:@turbot/gcp-cisv2-0#/policy/types/attestation"
+#   # value    = "90 days"
+#   value    = "1 year"
+#   # value    = "2 years"
+# }
