531314b912443392f80efd68a65ff7cebf9ac557
niharika2k00 <dniharika16@gmail.com>
2024-07-20 21:17:48 +0530

    Add 2 new pp

---
+++ policy_packs/aws/ec2/enforce_encryption_by_default_is_enabled_for_account_attributes/policies.tf
@@ -0,0 +1,14 @@
+# AWS > EC2 > Account Attributes > EBS Encryption by Default
+resource "turbot_policy_setting" "aws_ec2_account_attribute_encryption_by_default" {
+  resource = turbot_smart_folder.aws_ec2_encryption_by_default.id
+  type     = "tmod:@turbot/aws-ec2#/policy/types/ec2AccountAttributesEbsEncryptionByDefault"
+  value    = "Check: AWS managed key or higher"
+  # value    = "Check: AWS managed key"
+  # value    = "Check: AWS managed key or higher"
+  # value    = "Check: Customer managed key"
+  # value    = "Check: Encryption at Rest > Customer Managed Key"
+  # value    = "Enforce: AWS managed key"
+  # value    = "Enforce: AWS managed key or higher"
+  # value    = "Enforce: Customer managed key"
+  # value    = "Enforce: Encryption at Rest > Customer Managed Key"
+}
---
+++ policy_packs/aws/ec2/enforce_instances_to_use_specific_instance_types/policies.tf
@@ -0,0 +1,20 @@
+# AWS > EC2 > Instance > Approved
+resource "turbot_policy_setting" "aws_ec2_instance_approved" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-ec2#/policy/types/instanceApproved"
+  value    = "Check: Approved"
+  # value    = "Enforce: Stop unapproved"
+  # value    = "Enforce: Stop unapproved if new"
+  # value    = "Enforce: Delete unapproved if new"
+}
+
+# A list of instance types that the AWS EC2 instance is approved to use
+# AWS > EC2 > Instance > Approved > Instance Types
+resource "turbot_policy_setting" "aws_ec2_volume_approved_volume_types" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-ec2#/policy/types/instanceApprovedInstanceTypes"
+  value    = <<-EOT
+    - "t3.*"
+    - "m5.xlarge"
+  EOT
+}
