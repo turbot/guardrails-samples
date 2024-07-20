b796cdad3c4c60a33f3f5c9a04bf4979e04887b1
SumitPopat <sumit@turbot.com>
2024-07-20 18:15:04 +0530

    use EOT format for policy setting

--- policy_packs/aws/ssm/enforce_install_packages_on_instances_using_ssm_package_installer/policies.tf
+++ policy_packs/aws/ssm/enforce_install_packages_on_instances_using_ssm_package_installer/policies.tf
@@ -10,7 +10,7 @@ resource "turbot_policy_setting" "aws_ssm_stack" {
 resource "turbot_policy_setting" "ssm_stack_source" {
   resource = var.region_resource
   type     = "tmod:@turbot/aws-ssm#/policy/types/ssmStackSource"
-  value    = <<EOP
+  value    = <<-EOT
     # Creates a SSM Command Document which installs package on Linux Instance from OS package source repository
     resource "aws_ssm_document" "turbot_linux_package_installer_public_network_document" {
       name            = "Turbot-LinuxPackageInstaller-PublicNetwork"
@@ -40,7 +40,7 @@ resource "turbot_policy_setting" "ssm_stack_source" {
                     apt-get update
                     apt-get install -y {{ package }}
                   fi
-    EOC
+      EOC
     }
 
     # Creates a SSM Command Document which installs packages avaiable on S3 buckets.
@@ -83,7 +83,7 @@ resource "turbot_policy_setting" "ssm_stack_source" {
                   if [[ $OS =~ "Ubuntu" ]]; then
                     dpkg -i $packageFile
                   fi
-    EOC
+      EOC
     }
 
     # Creates a SSM Automation Document which installs package on Linux Instance inside a private network by storing target package on a S3 bucket
@@ -145,7 +145,7 @@ resource "turbot_policy_setting" "ssm_stack_source" {
                 - Key: tag:turbot:InventoryCollection
                   Values:
                     - "true"
-    EOC
+      EOC
     }
-EOP
+  EOT
 }
