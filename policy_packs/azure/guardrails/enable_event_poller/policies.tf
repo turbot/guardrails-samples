f4e010b9d83c80a2d0ecff127ffdfed6374c654e
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2024-07-23 02:47:29 +0530

    Add policy pack for Event Pollers for AWS/Azure/GCP. (#812)

---
+++ policy_packs/azure/guardrails/enable_event_poller/policies.tf
@@ -0,0 +1,20 @@
+# Azure > Turbot > Event Poller
+resource "turbot_policy_setting" "azure_turbot_event_poller" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/azure#/policy/types/eventPoller"
+  value    = "Enabled"
+}
+
+# Azure > Turbot > Event Poller > Interval
+resource "turbot_policy_setting" "azure_turbot_event_poller_interval" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/azure#/policy/types/eventPollerInterval"
+  value    = "Every 1 minute"
+}
+
+# Azure > Turbot > Event Poller > Interval > Window
+resource "turbot_policy_setting" "azure_turbot_event_poller_interval_window" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/azure#/policy/types/eventPollerWindow"
+  value    = "15 minutes"
+}
