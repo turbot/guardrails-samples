53da56639002cd8c99520b1a238fd7bb7a3a77b0
RahulLah19 <47312740+RahulLah19@users.noreply.github.com>
2024-07-23 02:24:45 +0530

    Add AWS VPC Policy Pack 5 (#811)

---
+++ policy_packs/aws/vpc/enforce_default_security_groups_to_not_exist/policies.tf
@@ -0,0 +1,48 @@
+# AWS > VPC > Security Group > Approved
+resource "turbot_policy_setting" "aws_vpc_security_group_approved" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-vpc-security#/policy/types/securityGroupApproved"
+  value    = "Check: Approved"
+  # value    = "Enforce: Delete unapproved if new"
+}
+
+# AWS > VPC > Security Group > Approved > Custom
+resource "turbot_policy_setting" "aws_vpc_security_group_approved_custom" {
+  resource       = turbot_policy_pack.main.id
+  type           = "tmod:@turbot/aws-vpc-security#/policy/types/securityGroupApprovedCustom"
+  template_input = <<-EOT
+    {   
+      resource {
+        name: get(path: "GroupName")
+      }
+    }
+  EOT
+  template = <<-EOT
+    {%- if $.resource.name == "default" -%}
+
+     {% set data = {
+          "title": "Default Security Group",
+          "result": "Not approved",
+          "message": "Default security group is not allowed to exist"
+      } -%}
+
+    {%- elif $.resource.name != "default" -%}
+
+     {% set data = {
+          "title": "Default Security Group",
+          "result": "Approved",
+          "message": "Not a default security group"
+      } -%}
+
+    {%- else -%}
+
+      {% set data = {
+          "title": "Default Security Group",
+          "result": "Skip",
+          "message": "No data for security group yet"
+      } -%}
+
+    {%- endif -%}
+    {{ data | json }}
+  EOT
+}
