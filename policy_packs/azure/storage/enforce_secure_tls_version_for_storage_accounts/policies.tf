a82d8744c3d597dee78406e97a809468309c6f0f
Jon Udell <jon@jonudell.info>
2024-09-04 11:30:31 -0700

    Update policy_packs/azure/storage/enforce_secure_tls_version_for_storage_accounts/policies.tf
    Co-authored-by: Sumit Popat <sumit@turbot.com>

--- policy_packs/azure/storage/enforce_secure_tls_version_for_storage_accounts/policies.tf
+++ policy_packs/azure/storage/enforce_secure_tls_version_for_storage_accounts/policies.tf
@@ -3,6 +3,5 @@ resource "turbot_policy_setting" "azure_storage_account_minimum_tls_version" {
   resource = turbot_policy_pack.main.id
   type     = "tmod:@turbot/azure-storage#/policy/types/storageAccountMinimumTlsVersion"
   value    = "Check: TLS 1.2"
-  # Uncomment the following line to enforce the policy
   # value    = "Enforce: TLS 1.2"
 }
