97d69c915d7f4a336be97af2921374d9c5d399fe
Venu <40973863+vkumbha@users.noreply.github.com>
2024-10-04 16:05:55 +0530

    Add policy pack - AWS > RDS > DB Snapshot [Manual] > Trusted Access. Closes #861 (#862)

---
+++ policy_packs/aws/rds/enforce_manual_db_instance_snapshots_are_shared_with_trusted_accounts/main.tf
@@ -0,0 +1,5 @@
+resource "turbot_policy_pack" "main" {
+  title       = "Enforce AWS RDS Manual DB Instance Snapshots Are Shared With Trusted Accounts"
+  description = "Ensure that sensitive data within snapshots is accessible only to authorized accounts, reducing the risk of unauthorized access and data breaches."
+  akas        = ["aws_rds_enforce_manual_db_instance_snapshots_are_shared_with_trusted_accounts"]
+}
