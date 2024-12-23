d804a57b2c3cc049485d38d5d59d65e68d6b6034
Venu <venu@turbot.com>
2024-12-23 15:23:38 +0530

    update title

--- policy_packs/azure/network/check_virtual_networks_have_flowlog_enabled/main.tf
+++ policy_packs/azure/network/check_virtual_networks_have_flowlog_enabled/main.tf
@@ -1,5 +1,5 @@
 resource "turbot_policy_pack" "main" {
-  title       = "Check Azure Virtual Networks Have Flow Log Enabled"
+  title       = "Check Azure Virtual Networks Have Flow Logs Enabled"
   description = "Ensure that logging is enabled for Azure Virtual Networks to monitor traffic and maintain security compliance."
   akas        = ["azure_network_check_virtual_networks_have_flowlog_enabled"]
 }
