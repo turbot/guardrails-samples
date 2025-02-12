0b41767a2a8be8306ab80ecedd46ceceda656986
Raj <raj@turbot.com>
2025-02-12 13:12:11 +0530

    Fix typo and content update in README (#920)

--- guardrails_utilities/shell_utils/run-policies/run-policies.sh
+++ guardrails_utilities/shell_utils/run-policies/run-policies.sh
@@ -2,7 +2,7 @@
 
 function displayTotalItems {
     local TOTAL_ITEMS=$1
-    
+
     echo "[INFO] Total amount of policies re-run: ${TOTAL_ITEMS}"
 }
 
@@ -25,9 +25,9 @@ function runPolicies {
     local INPUT_STRING=""
     local MUTATION_BODY=""
     local MUTATION_VARIABLES=""
-    
+
     let "END_INDEX = COLLECTION_COUNT - 1"
-    
+
     for (( INDEX=0; INDEX<${COLLECTION_COUNT}; INDEX++ ))
     do
         # Parameters
@@ -36,31 +36,31 @@ function runPolicies {
             INPUT_STRING+=", "
         fi
         INPUT_STRING+="\$input${INDEX}: RunPolicyInput!"
-        
+
         # Mutation
         MUTATION_BODY_ENTRY="run${INDEX}: runPolicy(input: \$input${INDEX}) { turbot { id } }"
         MUTATION_BODY+="${MUTATION_BODY_ENTRY} "
-        
+
         # Variables
         VARIABLE_ENTRY='"input'${INDEX}'": { "id": '${RUN_COLLECTION[${INDEX}]}' }'
         if [[ ${INDEX} != ${END_INDEX} ]]
         then
             VARIABLE_ENTRY+=","
         fi
-        
+
         MUTATION_VARIABLES+="${VARIABLE_ENTRY} "
     done
-    
+
     local MUTATION="mutation RunPolicies(${INPUT_STRING}) { ${MUTATION_BODY}}"
     local VARIABLES="{ ${MUTATION_VARIABLES}}"
-    
+
     if [[ -z ${PROFILE} ]]
     then
         local MUTATION_RESULT=$(turbot graphql --format json --query "'${MUTATION}'" --variables "'${VARIABLES}'")
     else
         local MUTATION_RESULT=$(turbot graphql --format json --query "'${MUTATION}'" --variables "'${VARIABLES}'" --profile "${PROFILE}")
     fi
-    
+
     for (( INDEX=0; INDEX<${COLLECTION_COUNT}; INDEX++ ))
     do
         local PROCESS_ID=$(echo ${MUTATION_RESULT} | jq '.run'${INDEX}'.turbot.id')
@@ -70,7 +70,7 @@ function runPolicies {
 
 function createTotalQuery {
     local FILTER=$1
-    
+
     local TOTAL_QUERY
     read -r -d '' TOTAL_QUERY <<-EOM
         query GetPoliciesTotal {
@@ -83,7 +83,7 @@ function createTotalQuery {
           }
         }
 EOM
-    
+
     echo ${TOTAL_QUERY}
 }
 
@@ -91,12 +91,12 @@ function createPolicyQuery {
     local FILTER=$1
     local PAGING=$2
     local BATCH_SIZE=$3
-    
+
     if [[ -z ${PAGING} ]]
     then
         PAGING='""'
     fi
-    
+
     local POLICIES_QUERY
     read -r -d '' POLICIES_QUERY <<-EOM
         query GetPolicies {
@@ -122,7 +122,7 @@ function createPolicyQuery {
           }
         }
 EOM
-    
+
     echo ${POLICIES_QUERY}
 }
 
@@ -130,9 +130,9 @@ function main {
     local TIME_SLEEP=20000
     local BATCH_SIZE=20
     local DRY_RUN=true
-    
+
     START=`date +%s`
-    
+
     # Parse the command line into values required by script
     while (( "$#" )); do
         case "$1" in
@@ -161,11 +161,11 @@ function main {
             -d|--dry-run)
                 if [[ -n "$2" ]] && [[ ${2:0:1} != "-" ]]
                 then
-                    if [[ ${2,,} = "false" ]]
+                    if [[ ${2} = "false" ]]
                     then
                         local DRY_RUN=false
                     fi
-                    
+
                     shift 2
                 else
                     echo "[ERROR] Argument for $1 is missing" >&2
@@ -212,16 +212,16 @@ function main {
             ;;
         esac
     done
-    
+
     if [[ -z ${FILTER} ]]
     then
         echo '[ERROR] Expected argument `--filter`' >&2
         displayHelp
         exit 2
     fi
-    
+
     EXPECTED_COMMANDS=( turbot jq )
-    
+
     for EXPECTED_COMMAND in "${EXPECTED_COMMANDS[@]}"
     do
         if ! command -v ${EXPECTED_COMMAND} &> /dev/null
@@ -230,19 +230,19 @@ function main {
             exit
         fi
     done
-    
+
     local TOTAL_QUERY_RESULT=""
     local TOTAL_QUERY=$(createTotalQuery "${FILTER}")
-    
+
     if [[ -z ${PROFILE} ]]
     then
         TOTAL_QUERY_RESULT=$(turbot graphql --format json --query "${TOTAL_QUERY}")
     else
         TOTAL_QUERY_RESULT=$(turbot graphql --format json --query "${TOTAL_QUERY}" --profile "${PROFILE}")
     fi
-    
+
     local TOTAL_POLICIES=$(echo ${TOTAL_QUERY_RESULT} | jq '.policyValues.metadata.stats.total')
-    
+
     if ! [[ $TOTAL_POLICIES =~ ^[0-9]+$ ]]
     then
         echo '[ERROR] GraphQL returned from server did not match expected results' >&2
@@ -250,26 +250,26 @@ function main {
         echo -e ${TOTAL_QUERY_RESULT}
         exit 3
     fi
-    
+
     local TOTAL_RETURNED=0
     local PAGING=""
-    
+
     while (( ${TOTAL_RETURNED} < ${TOTAL_POLICIES} ))
     do
         local POLICIES_QUERY_RESULT=""
         let "OPTIMISED_BATCH_SIZE = TOTAL_POLICIES - TOTAL_RETURNED < BATCH_SIZE ? TOTAL_POLICIES - TOTAL_RETURNED : BATCH_SIZE "
         local POLICIES_QUERY=$(createPolicyQuery "${FILTER}" "${PAGING}" ${OPTIMISED_BATCH_SIZE} )
-        
+
         if [[ -z ${PROFILE} ]]
         then
             POLICIES_QUERY_RESULT=$(turbot graphql --format json --query "${POLICIES_QUERY}")
         else
             POLICIES_QUERY_RESULT=$(turbot graphql --format json --query "${POLICIES_QUERY}" --profile "${PROFILE}")
         fi
-        
+
         local TOTAL_ITEMS=$(echo ${POLICIES_QUERY_RESULT} | jq ".policyValues.items | length")
         local RUN_COLLECTION=()
-        
+
         for ((INDEX = 0 ; INDEX < TOTAL_ITEMS ; INDEX++)); do
             local ITEM=$(echo ${POLICIES_QUERY_RESULT} | jq ".policyValues.items[${INDEX}]")
             local STATE=$(echo ${ITEM} | jq ".state")
@@ -277,38 +277,38 @@ function main {
             local RESOURCE=$(echo ${ITEM} | jq ".resource.trunk.title")
             local TYPE=$(echo ${ITEM} | jq ".type.title")
             local ID=$(echo ${ITEM} | jq ".turbot.id")
-            
+
             echo "[INFO] Policy $(( INDEX + TOTAL_RETURNED + 1 )) of ${TOTAL_POLICIES}"
             echo "[INFO]    Type: ${TYPE}"
             echo "[INFO]    Resource: ${RESOURCE}"
             echo "[INFO]    State: ${STATE}"
             echo "[INFO]    Reason: ${REASON}"
             echo "[INFO]    ID: ${ID}"
-            
+
             RUN_COLLECTION+=(${ID})
         done
-        
+
         if [[ ${DRY_RUN} == false ]]
         then
             runPolicies "${RUN_COLLECTION[@]}"
         fi
-        
+
         let "TOTAL_RETURNED += TOTAL_ITEMS"
-        
+
         if [[ ${DRY_RUN} == false ]] && (( ${TOTAL_RETURNED} != ${TOTAL_ITEMS} ))
         then
             echo "[INFO] Backing off for" $((TIME_SLEEP / 1000)) "second(s)"
             sleep $((TIME_SLEEP / 1000))
         fi
-        
+
         PAGING=$(echo ${POLICIES_QUERY_RESULT} | jq ".policyValues.paging.next")
     done
-    
+
     displayTotalItems ${TOTAL_RETURNED}
-    
+
     END=`date +%s`
     RUNTIME=$((END - START))
-    
+
     echo "[INFO] Total time taken ${RUNTIME} second(s)"
 }
 
