487f7c0a8c8a4b9b6eb38974640f39ff38563446
Venu <venu@turbot.com>
2025-05-21 15:31:35 +0530

    update the providers.tf

--- policy_packs/aws/stack/deploy_vpc_security_group_stack/providers.tf
+++ policy_packs/aws/stack/deploy_vpc_security_group_stack/providers.tf
@@ -3,18 +3,5 @@ terraform {
     turbot = {
       source = "turbot/turbot"
     }
-    # aws = {
-    #   source  = "terraform-providers/aws"
-    #   version = "5.72.0"
-    # }
   }
-}
-
-provider "turbot" {
-  profile = "punisher"
-}
-
-provider "aws" {
-  profile = "punisher"
-  region  = "us-east-1"
 }
