a80c2d799cf1822bda2e3f64f834f223892178d1
Venu <40973863+vkumbha@users.noreply.github.com>
2025-06-05 11:15:05 +0530

    Add policy pack: Enforce AWS Load Balancer Listeners Do Not Use HTTP (#940)

---
+++ policy_packs/aws/ec2/enforce_load_balancer_listeners_do_not_use_http/policies.tf
@@ -0,0 +1,32 @@
+# AWS > EC2 > Load Balancer Listener > Approved
+resource "turbot_policy_setting" "aws_ec2_load_balancer_listener_approved" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-ec2#/policy/types/loadBalancerListenerApproved"
+  value    = "Check: Approved"
+  # value    = "Enforce: Delete unapproved if new"
+}
+
+# AWS > EC2 > Load Balancer Listener > Approved > Protocols
+resource "turbot_policy_setting" "aws_ec2_load_balancer_listener_approved_protocols_internet_facing" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-ec2#/policy/types/loadBalancerListenerApprovedProtocols"
+  value    = <<-EOT
+    - HTTPS
+    - TCP
+    - TLS
+    - UDP
+    - TCP_UDP
+    EOT
+}
+
+# AWS > EC2 > Load Balancer Listener > Approved > Ports
+resource "turbot_policy_setting" "aws_ec2_load_balancer_listener_approved_ports" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/aws-ec2#/policy/types/loadBalancerListenerApprovedPorts"
+  value    = <<-EOT
+    # HTTPS port
+    - "443"
+    # Registered ports
+    - "1024-49151"
+    EOT
+}
