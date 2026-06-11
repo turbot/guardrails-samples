8d67a58453a8148b6e46c0141d5c457f446a6998
Raj <raj@turbot.com>
2026-06-11 18:57:00 +0530

    Add Azure Search Service public network access policy pack (#989)

---
+++ policy_packs/azure/searchmanagement/enforce_public_network_access_disabled_for_search_services/policies.tf
@@ -0,0 +1,7 @@
+# Azure > Search Management > Search Service > Public Network Access
+resource "turbot_policy_setting" "azure_searchmanagement_search_service_public_network_access" {
+  resource = turbot_policy_pack.main.id
+  type     = "tmod:@turbot/azure-searchmanagement#/policy/types/searchServicePublicNetworkAccess"
+  value    = "Check: Disabled"
+  # value    = "Enforce: Disabled"
+}
